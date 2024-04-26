334b5fb03eb69548d26e1eb2ea03d4a9539e32c0
letonghan <letong.han@intel.com>
2024-04-26 16:44:14 +0800

    add data layer for microservices, add embedding example service
    Signed-off-by: letonghan <letong.han@intel.com>

--- GenAIComps/embedding/langchain/embedding.py
+++ GenAIComps/embedding/langchain/embedding.py
@@ -1,61 +1,37 @@
-# Copyright (c) 2024 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os
-
-from fastapi import APIRouter, FastAPI, Request
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings, HuggingFaceHubEmbeddings
-from starlette.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
-)
+import asyncio
+from http_service import HTTPService
+from _docarray import TextDoc, EmbedDoc1024
+from docarray.base_doc import DocArrayResponse
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
 
-class EmbeddingRouter(APIRouter):
+async def setup():
+    runtime_args = {
+        'title': 'test_service',
+        'description': "this is a test.",
+        'protocol': 'http',
+        'port': 8099,
+        'host': 'localhost',
+    }
+    # breakpoint()
+    service = HTTPService(runtime_args=runtime_args, cors=False)
+    app = service.app
 
-    def __init__(self, tei_endpoint=None) -> None:
-        super().__init__()
+    @app.post(
+        path="/v1/embed",
+        response_model=EmbedDoc1024,
+        response_class=DocArrayResponse,
+        summary="Get the embeded vector of the input text",
+        tags=["Debug"],
+    )
+    def embedding(input: TextDoc) -> EmbedDoc1024:
+        embeddings = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-large-en-v1.5")
+        embed_vector = embeddings.embed_query(input.text)
+        res = EmbedDoc1024(text=input.text, embedding=embed_vector)
+        return res
 
-        # Define LLM Chain
-        if tei_endpoint:
-            # create embeddings using TEI endpoint service
-            self.embeddings = HuggingFaceHubEmbeddings(model=tei_endpoint)
-        else:
-            # create embeddings using local embedding model
-            self.embeddings = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-large-en-v1.5")
-
-
-tei_embedding_endpoint = os.getenv("TEI_ENDPOINT")
-router = EmbeddingRouter(tei_embedding_endpoint)
-
-
-@router.post("/v1/embeddings")
-async def rag_chat(request: Request):
-    params = await request.json()
-    print(f"[embeddings] POST request: /v1/embeddings, params:{params}")
-    query = params["query"]
-
-    embeddings = router.embeddings(query)
-    return embeddings
+    await service.initialize_server()
+    await service.execute_server()
 
 
-app.include_router(router)
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=7000)
+asyncio.run(setup())
