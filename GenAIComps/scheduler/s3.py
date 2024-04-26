d9364015f7bb36817b9748aa2d2766c6240726f6
Spycsh <sihan.chen@intel.com>
2024-04-26 03:06:59 -0700

    add basic DAG flow

---
+++ GenAIComps/scheduler/s3.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+
+import uvicorn
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import Response,JSONResponse
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
+
+@app.get("/v1/health")
+async def health() -> Response:
+    """Health check."""
+    print("***** in s4 *****")
+    # return Response(status_code=200)
+    return {"data": "***** in s4 *****"}
+
+@app.post("/v1/add")
+async def add(request: Request) -> JSONResponse:
+    req = await request.json()
+    number = req["number"]
+    number += 1
+    return {"number": number}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=8008)
+    parser.add_argument("--device", type=str, default="cpu")
+
+    args = parser.parse_args()
+
+    uvicorn.run(app, host=args.host, port=args.port)
