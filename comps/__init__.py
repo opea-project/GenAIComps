f3f8226073166abdb9daa78fceb2a241c451ccfe
lvliang-intel <liang1.lv@intel.com>
2024-05-09 11:50:28 +0800

    fix ut issues
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- comps/__init__.py
+++ comps/__init__.py
@@ -31,4 +31,4 @@ from comps.cores.mega.orchestrator_with_yaml import ServiceOrchestratorWithYaml
 from comps.cores.mega.micro_service import MicroService, RemoteMicroService, register_microservice, opea_microservices
 
 # Telemetry
-from comps.cores.telemetry.opea_telemetry import opea_telemetry
+from comps.cores.mega.telemetry.opea_telemetry import opea_telemetry
