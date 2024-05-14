d1d2bdfea018ce32738759da1dc7d0b67a77ef63
lvliang-intel <liang1.lv@intel.com>
2024-05-14 23:52:36 +0800

    fix ut issue
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- comps/cores/mega/gateway.py
+++ comps/cores/mega/gateway.py
@@ -25,9 +25,14 @@ from ..proto.api_protocol import (
     ChatMessage,
     UsageInfo,
 )
-
+, , 
 class Gateway:
-    def __init__(self, megaservice, host, port, endpoint, input_datatype, output_datatype):
+    def __init__(self, megaservice,
+                 host = "0.0.0.0",
+                 port = 8888, 
+                 endpoint=str(MegaServiceEndpoint.CHAT_QNA),
+                 input_datatype=ChatCompletionRequest,
+                 output_datatype=ChatCompletionResponse):
         self.megaservice = megaservice
         self.host = host
         self.port = port
