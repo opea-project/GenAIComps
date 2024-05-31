af1caf8981f6738ce3fb9e8f5daf57f16d40640c
Zahidul Haque <zahidul.haque@intel.com>
2024-05-31 09:48:53 +0530

    Fixed review comments
    Signed-off-by: Zahidul Haque <zahidul.haque@intel.com>

--- comps/llms/vllm-openvino/launch_model_server.sh
+++ comps/llms/vllm-openvino/launch_model_server.sh
@@ -15,23 +15,42 @@
 # limitations under the License.
 
 # Set default values
+
+
 default_port=8000
 default_model="meta-llama/Llama-2-7b-hf"
 swap_space=50
 
+while getopts ":hm:p:" opt; do
+  case $opt in
+    h)
+      echo "Usage: $0 [-h] [-m model] [-p port]"
+      echo "Options:"
+      echo "  -h         Display this help message"
+      echo "  -m model   Model (default: meta-llama/Llama-2-7b-hf)"
+      echo "  -p port    Port (default: 8000)"
+      exit 0
+      ;;
+    m)
+      model=$OPTARG
+      ;;
+    p)
+      port=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
 # Assign arguments to variables
-port_number=${1:-$default_port}
-model_name=${2:-$default_model}
+model_name=${model:-$default_model}
+port_number=${port:-$default_port}
 
-
-# Check if all required arguments are provided
-if [ "$#" -lt 0 ] || [ "$#" -gt 2 ]; then
-    echo "Usage: $0 [port_number] [model_name]"
-    exit 1
-fi
 
 # Set the Huggingface cache directory variable
 HF_CACHE_DIR=$HOME/.cache/huggingface
 
-# Start the model server with openvino as the backened inference server
+# Start the model server using Openvino as the backend inference engine. Provide the container name that is unique and meaningful, typically one that includes the model name.
 docker run --rm --name="vllm-openvino-server" -p $port_number:$port_number -v $HF_CACHE_DIR:/root/.cache/huggingface vllm:openvino --model $model_name --port $port_number --disable-log-requests --swap-space $swap_space
