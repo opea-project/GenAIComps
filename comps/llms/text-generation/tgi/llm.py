535f33978b162361ec45a2f79d8fbd7ea28948ff
lvliang-intel <liang1.lv@intel.com>
2024-06-04 08:28:08 +0800

    Fix RAG performance issues
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- comps/llms/text-generation/tgi/llm.py
+++ comps/llms/text-generation/tgi/llm.py
@@ -19,21 +19,7 @@ from comps import GeneratedDoc, LLMParamsDoc, ServiceType, opea_microservices, r
 )
 @traceable(run_type="llm")
 def llm_generate(input: LLMParamsDoc):
-    llm_endpoint = os.getenv("TGI_LLM_ENDPOINT", "http://localhost:8080")
-    llm = HuggingFaceEndpoint(
-        endpoint_url=llm_endpoint,
-        max_new_tokens=input.max_new_tokens,
-        top_k=input.top_k,
-        top_p=input.top_p,
-        typical_p=input.typical_p,
-        temperature=input.temperature,
-        repetition_penalty=input.repetition_penalty,
-        streaming=input.streaming,
-        timeout=600,
-    )
-
     if input.streaming:
-
         async def stream_generator():
             chat_response = ""
             async for text in llm.astream(input.query):
@@ -51,4 +37,17 @@ def llm_generate(input: LLMParamsDoc):
 
 
 if __name__ == "__main__":
+    llm_endpoint = os.getenv("TGI_LLM_ENDPOINT", "http://localhost:8080")
+    llm = HuggingFaceEndpoint(
+        endpoint_url=llm_endpoint,
+        max_new_tokens=input.max_new_tokens,
+        top_k=input.top_k,
+        top_p=input.top_p,
+        typical_p=input.typical_p,
+        temperature=input.temperature,
+        repetition_penalty=input.repetition_penalty,
+        streaming=input.streaming,
+        timeout=600,
+    )
+
     opea_microservices["opea_service@llm_tgi"].start()
