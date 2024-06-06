2a7c7298d008bba9e17f1349f69872baa063fe33
tianyil1 <tianyi.liu@intel.com>
2024-06-06 13:48:45 +0800

    updated the gaudi version with the official version
    Signed-off-by: tianyil1 <tianyi.liu@intel.com>

--- comps/llms/text-generation/vllm/launch_vllm_service.sh
+++ comps/llms/text-generation/vllm/launch_vllm_service.sh
@@ -7,7 +7,7 @@
 # Set default values
 default_port=8080
 default_hw_mode="cpu"
-default_model="mistralai/Mistral-7B-v0.1"
+default_model="Intel/neural-chat-7b-v3-3"
 
 # Assign arguments to variables
 port_number=${1:-$default_port}
@@ -18,7 +18,7 @@ hw_mode=${3:-$default_hw_mode}
 if [ "$#" -lt 0 ] || [ "$#" -gt 3 ]; then
     echo "Usage: $0 [port_number] [model_name] [hw_mode]"
     echo "port_number: The port number assigned to the vLLM CPU endpoint, with the default being 8080."
-    echo "model_name: The model name utilized for LLM, with the default set to 'mistralai/Mistral-7B-v0.1'."
+    echo "model_name: The model name utilized for LLM, with the default set to 'Intel/neural-chat-7b-v3-3'."
     echo "hw_mode: The hardware mode utilized for LLM, with the default set to 'cpu', and the optional selection can be 'hpu'"
     exit 1
 fi
@@ -28,7 +28,7 @@ volume=$PWD/data
 
 # Build the Docker run command based on hardware mode
 if ["$hw_mode" = "hpu"]; then
-    docker run -it --runtime=habana --rm --name="ChatQnA_server" -p $port_number:$port_number -e HABANA_VISIBLE_DEVICES=all -e OMPI_MCA_btl_vader_single_copy_mechanism=none --cap-add=sys_nice --ipc=host -e HTTPS_PROXY=$https_proxy -e HTTP_PROXY=$https_proxy -e HF_TOKEN=${HUGGINGFACEHUB_API_TOKEN} vllm:hpu /bin/bash -c "export VLLM_CPU_KVCACHE_SPACE=40 && python3 -m vllm.entrypoints.openai.api_server --enforce-eager --model $model_name --host 0.0.0.0 --port $port_number"
+    docker run -it --runtime=habana --rm --name="ChatQnA_server" -p $port_number:$port_number -e HABANA_VISIBLE_DEVICES=all -e OMPI_MCA_btl_vader_single_copy_mechanism=none --cap-add=sys_nice --ipc=host -e HTTPS_PROXY=$https_proxy -e HTTP_PROXY=$https_proxy -e HF_TOKEN=${HF_TOKEN} vllm:hpu /bin/bash -c "export VLLM_CPU_KVCACHE_SPACE=40 && python3 -m vllm.entrypoints.openai.api_server --enforce-eager --model $model_name --host 0.0.0.0 --port $port_number"
 else
     docker run -it --rm --name="ChatQnA_server" -p $port_number:$port_number --network=host -v $volume:/data -e HTTPS_PROXY=$https_proxy -e HTTP_PROXY=$https_proxy -e HF_TOKEN=${HUGGINGFACEHUB_API_TOKEN} vllm:cpu /bin/bash -c "cd / && export VLLM_CPU_KVCACHE_SPACE=40 && python3 -m vllm.entrypoints.openai.api_server --model $model_name --host 0.0.0.0 --port $port_number"
 fi
