145bcf00a7c47acbb408b3b1d24b242c3d1a6d9b
letonghan <letong.han@intel.com>
2024-06-12 17:10:41 +0800

    fix ut issue
    Signed-off-by: letonghan <letong.han@intel.com>

--- tests/test_embeddings_llama_index.sh
+++ tests/test_embeddings_llama_index.sh
@@ -11,15 +11,14 @@ ip_address=$(hostname -I | awk '{print $1}')
 function build_docker_images() {
     cd $WORKPATH
     echo $(pwd)
-    docker build --no-cache -t opea/embedding-tei:comps -f comps/embeddings/llama_index/docker/Dockerfile .
+    docker build --no-cache -t opea/embedding-tei:comps --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/embeddings/llama_index/docker/Dockerfile .
 }
 
 function start_service() {
     tei_endpoint=5001
     model="BAAI/bge-large-en-v1.5"
     revision="refs/pr/5"
-    unset http_proxy
-    docker run -d --name="test-comps-embedding-tei-endpoint" -p $tei_endpoint:80 -v ./data:/data --pull always ghcr.io/huggingface/text-embeddings-inference:cpu-1.2 --model-id $model --revision $revision
+    docker run -d --name="test-comps-embedding-tei-endpoint" -p $tei_endpoint:80 -v ./data:/data -e http_proxy=$http_proxy -e https_proxy=$https_proxy --pull always ghcr.io/huggingface/text-embeddings-inference:cpu-1.2 --model-id $model --revision $revision
     export TEI_EMBEDDING_ENDPOINT="http://${ip_address}:${tei_endpoint}"
     tei_service_port=5010
     docker run -d --name="test-comps-embedding-tei-server" -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p ${tei_service_port}:6000 --ipc=host -e TEI_EMBEDDING_ENDPOINT=$TEI_EMBEDDING_ENDPOINT  opea/embedding-tei:comps
@@ -33,9 +32,9 @@ function validate_microservice() {
     HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST -d '{"text":"What is Deep Learning?"}' -H 'Content-Type: application/json' "$URL")
     if [ "$HTTP_STATUS" -eq 200 ]; then
         echo "[ embedding - llama_index ] HTTP status is 200. Checking content..."
-        local CONTENT=$(curl -s -X POST -d "$INPUT_DATA" -H 'Content-Type: application/json' "$URL" | tee ${LOG_PATH}/embedding.log)
+        local CONTENT=$(curl -s -X POST -d '{"text":"What is Deep Learning?"}' -H 'Content-Type: application/json' "$URL" | tee ${LOG_PATH}/embedding.log)
 
-        if echo " " | grep -q "$EXPECTED_RESULT"; then
+        if echo '"text":"What is Deep Learning?","embedding":\[' | grep -q "$EXPECTED_RESULT"; then
             echo "[ embedding - llama_index ] Content is as expected."
         else
             echo "[ embedding - llama_index ] Content does not match the expected result: $CONTENT"
