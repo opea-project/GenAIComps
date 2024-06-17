d3930bb23c5e35407da0ffd7fd171b4d390bb1e2
chensuyue <suyue.chen@intel.com>
2024-06-18 00:07:28 +0800

    Enable microservice docker image auto build and push
    Signed-off-by: chensuyue <suyue.chen@intel.com>

---
+++ .github/workflows/scripts/docker_images_build_push.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+set -xe
+
+WORKSPACE=$(dirname "$PWD")
+IMAGE_REPO=${IMAGE_REPO:-$OPEA_IMAGE_REPO}
+IMAGE_TAG=${IMAGE_TAG:-latest}
+micro_service=$1
+
+function docker_build() {
+    # docker_build <IMAGE_NAME>
+    IMAGE_NAME=$1
+    dockerfile_path=${WORKSPACE}/comps/${micro_service}
+    if [ -f "$dockerfile_path/Dockerfile" ]; then
+        DOCKERFILE_PATH="$dockerfile_path/Dockerfile"
+    elif [ -f "$dockerfile_path/docker/Dockerfile" ]; then
+        DOCKERFILE_PATH="$dockerfile_path/docker/Dockerfile"
+    else
+        echo "Dockerfile not found"
+        exit 1
+    fi
+    echo "Building ${IMAGE_REPO}/${IMAGE_NAME}:$IMAGE_TAG using Dockerfile $DOCKERFILE_PATH"
+
+    docker build --no-cache -t ${IMAGE_REPO}/${IMAGE_NAME}:$IMAGE_TAG -f $DOCKERFILE_PATH .
+    docker push ${IMAGE_REPO}opea/$1:$IMAGE_TAG
+    docker rmi ${IMAGE_REPO}opea/$1:$IMAGE_TAG
+}
+
+# $1 is like "llms_text-generation_tgi"
+case ${micro_service} in
+    "asr"|"tts")
+        IMAGE_NAME="opea/${micro_service}"
+        ;;
+    "embeddings/langchain")
+        IMAGE_NAME="opea/embedding-tei"
+        ;;
+    "retrievers/langchain")
+        IMAGE_NAME="opea/retriever-redis"
+        ;;
+    *)
+        echo "Not supported yet"
+        ;;
+esac
+
+docker_build "${IMAGE_NAME}"
