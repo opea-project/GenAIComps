8b76d391be750b5c2e43febda415b94761e4a705
Yogesh <yogesh.pandey@intel.com>
2024-06-23 00:39:33 +0530

    Initial Implementation
    Signed-off-by: Yogesh Pandey <yogesh.pandey@intel.com>

---
+++ comps/dbconnector/mongo/config.py
@@ -0,0 +1,7 @@
+﻿import os
+
+# MONGO configuration
+MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+MONGO_PORT = os.getenv("MONGO_PORT", 27017)
+DB_NAME = os.getenv("DB_NAME", "chat")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "chat")
