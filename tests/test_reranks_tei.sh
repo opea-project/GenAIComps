7847ceb24ce45973467c428e4520fcbf6b622ac2
chensuyue <suyue.chen@intel.com>
2024-07-09 22:33:43 +0800

    update reranks tei test
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- tests/test_reranks_tei.sh
+++ tests/test_reranks_tei.sh
@@ -8,7 +8,7 @@ WORKPATH=$(dirname "$PWD")
 ip_address=$(hostname -I | awk '{print $1}')
 function build_docker_images() {
     cd $WORKPATH
-    docker build --no-cache -t opea/reranking-tei:comps -f comps/reranks/langchain/docker/Dockerfile .
+    docker build --no-cache -t opea/reranking-tei:comps -f comps/reranks/tei/docker/Dockerfile .
 }
 
 function start_service() {
