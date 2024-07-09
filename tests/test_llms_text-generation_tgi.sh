fdc1612d7aeeea30fe741c10e76425b0a3d94063
chensuyue <suyue.chen@intel.com>
2024-07-09 22:27:18 +0800

    add model for test
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- tests/test_llms_text-generation_tgi.sh
+++ tests/test_llms_text-generation_tgi.sh
@@ -58,10 +58,10 @@ function main() {
     build_docker_images
 
     llm_models=(
-    # Intel/neural-chat-7b-v3-3
+    Intel/neural-chat-7b-v3-3
     meta-llama/Llama-2-7b-chat-hf
     meta-llama/Meta-Llama-3-8B-Instruct
-    # microsoft/Phi-3-mini-4k-instruct
+    microsoft/Phi-3-mini-4k-instruct
     )
     for model in "${llm_models[@]}"; do
       start_service "${model}"
