9e35404b0fc045283a91321c50f8e7ecc280cd11
jmansdorfer <jmansdorfer19@gmail.com>
2024-07-12 15:40:55 -0400

    adding inital scripts for pg guardrails
    Signed-off-by: Daniel Whitenack <whitenack.daniel@gmail.com>

---
+++ comps/guardrails/pii_detection/predictinguard/pii_predictionguard.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2024 Prediction Guard, Inc.
+# SPDX-License-Identified: Apache-2.0
+
+from predictionguard import PredictionGuard
+
+from comps import (
+    ServiceType, 
+    TextDoc, 
+    opea_microservices, 
+    register_microservice, 
+    register_statistics,
+    statistics_dict
+)
+
+
+@register_microservice(
+    name="opea_service@pii_predictionguard",
+    service_type=ServiceType.GUARDRAIL,
+    endpoint="/v1/guardrails",
+    host="0.0.0.0",
+    post="9080",
+    input_datatype=TextDoc,
+    output_datatype=TextDoc
+)
+
+@register_statistics(names="opea_service@pii_predictionguard")
+def pii_guard(input: TextDoc) -> TextDoc:
+    client = PredictionGuard()
+
+    prompt = input.prompt
+    replace = input.replace
+    replace_method = input.replace_method
+
+    result = client.injection.check(
+        prompt=prompt,
+        replace=replace,
+        replace_method = replace_method
+    )
+
+    return TextDoc(text=result["checks"][0]["new_prompt"])
+
+
+if __name__ == "__main__":
+    print("Prediction Guard PII Detection initialized.")
+    opea_microservices["opea_service@pii_predictionguard"].start()
---
+++ comps/guardrails/prompt_injection/injection_predictionguard.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2024 Prediction Guard, Inc.
+# SPDX-License-Identified: Apache-2.0
+
+from predictionguard import PredictionGuard
+
+from comps import (
+    ServiceType, 
+    TextDoc, 
+    opea_microservices, 
+    register_microservice, 
+    register_statistics,
+    statistics_dict
+)
+
+
+@register_microservice(
+    name="opea_service@injection_predictionguard",
+    service_type=ServiceType.GUARDRAIL,
+    endpoint="/v1/guardrails",
+    host="0.0.0.0",
+    post="9085",
+    input_datatype=TextDoc,
+    output_datatype=TextDoc
+)
+
+@register_statistics(names="opea_service@injection_predictionguard")
+def injection_guard(input: TextDoc) -> TextDoc:
+    client = PredictionGuard()
+
+    prompt = input.prompt
+    detect = input.detect
+
+    result = client.injection.check(
+        prompt=prompt,
+        detect=detect
+    )
+
+    return TextDoc(text=result["checks"][0]["probability"])
+
+
+if __name__ == "__main__":
+    print("Prediction Guard Injection Detection initialized.")
+    opea_microservices["opea_service@injection_predictionguard"].start()
