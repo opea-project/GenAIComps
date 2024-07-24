b7cd748200b50794945c87cd1429254ba9e224ea
Berke <berkecanrizai1@gmail.com>
2024-07-24 13:23:33 +0200

    fix: formatting, data sources
    Signed-off-by: Berke <berkecanrizai1@gmail.com>

--- comps/retrievers/langchain/pathway/pathway_vs.py
+++ comps/retrievers/langchain/pathway/pathway_vs.py
@@ -3,7 +3,10 @@ import os
 
 import pathway as pw
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings, HuggingFaceHubEmbeddings
+from langchain_community.embeddings import (
+    HuggingFaceBgeEmbeddings,
+    HuggingFaceHubEmbeddings,
+)
 from pathway.xpacks.llm.parsers import ParseUnstructured
 from pathway.xpacks.llm.vector_store import VectorStoreServer
 
@@ -20,6 +23,8 @@ data = pw.io.fs.read(
     with_metadata=True,
 )
 
+data_sources = [data]
+
 splitter = CharacterTextSplitter()
 
 host = os.getenv("PATHWAY_HOST", "127.0.0.1")
@@ -27,24 +32,30 @@ port = int(os.getenv("PATHWAY_PORT", 8666))
 
 EMBED_MODEL = os.getenv("EMBED_MODEL", "BAAI/bge-base-en-v1.5")
 
-tei_embedding_endpoint = os.getenv("TEI_EMBEDDING_ENDPOINT")  #  == 10
+tei_embedding_endpoint = os.getenv("TEI_EMBEDDING_ENDPOINT")
 
 if __name__ == "__main__":
     # Create vectorstore
     if tei_embedding_endpoint:
         # create embeddings using TEI endpoint service
+        logging.info(
+            f"Initializing the embedder from tei_embedding_endpoint: {tei_embedding_endpoint}"
+        )
         embeddings = HuggingFaceHubEmbeddings(model=tei_embedding_endpoint)
     else:
         # create embeddings using local embedding model
         embeddings = HuggingFaceBgeEmbeddings(model_name=EMBED_MODEL)
 
-
-    print(tei_embedding_endpoint)
-    print(embeddings.embed_query("abc"))
-
     server = VectorStoreServer.from_langchain_components(
-        data, embedder=embeddings, parser=ParseUnstructured(), splitter=splitter
+        *data_sources,
+        embedder=embeddings,
+        parser=ParseUnstructured(),
+        splitter=splitter,
     )
+
     server.run_server(
-        host, port=port, with_cache=True, cache_backend=pw.persistence.Backend.filesystem("./Cache")
+        host,
+        port=port,
+        with_cache=True,
+        cache_backend=pw.persistence.Backend.filesystem("./Cache"),
     )
