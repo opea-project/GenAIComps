879ae8dd4f863be8bfce5d83083b3c6f53a4c2d8
Chun Tao <chun.tao@intel.com>
2024-07-30 16:43:23 -0700

    update
    Signed-off-by: Chun Tao <chun.tao@intel.com>

--- comps/animation/test_animation_server.py
+++ comps/animation/test_animation_server.py
@@ -5,6 +5,6 @@ import json
 import requests
 
 endpoint = "http://localhost:7860/v1/animation"
-inputs = {"image": "", "audio": ""}
+inputs = {"audio_base64_byte_str": "", "image_base64_byte_str": ""}
 response = requests.post(url=endpoint, data=json.dumps(inputs), proxies={"http": None})
 print(response.json())
