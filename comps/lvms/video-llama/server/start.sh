3117dd14d65763f54907137e28f89acde3e57172
BaoHuiling <huiling.bao@intel.com>
2024-08-02 11:31:41 +0800

    update video llama, model download
    Signed-off-by: BaoHuiling <huiling.bao@intel.com>

--- comps/lvms/video-llama/server/start.sh
+++ comps/lvms/video-llama/server/start.sh
@@ -1,7 +1,20 @@
 # /bin/bash
 # Download models
 MODEL_REPO=https://huggingface.co/DAMO-NLP-SG/Video-LLaMA-2-7B-Finetuned
-echo "Please wait for model download..."
-git lfs install &&  git clone ${MODEL_REPO} /home/user/model/Video-LLaMA-2-7B-Finetuned
-# rm Video-LLaMA-2-7B-Finetuned/AL*.pth Video-LLaMA-2-7B-Finetuned/imagebind_huge.pth
+llm_download=${llm_download}
+echo "llm_download: ${llm_download}"
+if [ "$llm_download" = "True" ]; then
+  # clean if exists
+  rm -rf /home/user/model/Video-LLaMA-2-7B-Finetuned
+
+  echo "Please wait for model download..."
+  git lfs install &&  git clone ${MODEL_REPO} /home/user/model/Video-LLaMA-2-7B-Finetuned
+  # rm Video-LLaMA-2-7B-Finetuned/AL*.pth Video-LLaMA-2-7B-Finetuned/imagebind_huge.pth
+elif [ "$llm_download" = "False" ]; then
+  echo "No model download"
+else
+  echo "llm_download should be True or False"
+  exit 1
+fi
+
 python server.py
