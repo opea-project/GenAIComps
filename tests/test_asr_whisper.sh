1a6ec9ba5feaa7e4bc7ebc180b5582f9b84ae80b
zepan <ze.pan@intel.com>
2024-08-09 23:26:41 +0800

    fix port
    Signed-off-by: zepan <ze.pan@intel.com>

--- tests/test_asr_whisper.sh
+++ tests/test_asr_whisper.sh
@@ -17,12 +17,12 @@ function build_docker_images() {
 function start_service() {
     unset http_proxy
     docker run -d --name="test-comps-asr-whisper" -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p 7066:7066 --ipc=host opea/whisper:comps
-    docker run -d --name="test-comps-asr" -e ASR_ENDPOINT=http://$ip_address:7066 -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p 9099:9099 --ipc=host opea/asr:comps
+    docker run -d --name="test-comps-asr" -e ASR_ENDPOINT=http://$ip_address:7066 -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p 9089:9099 --ipc=host opea/asr:comps
     sleep 3m
 }
 
 function validate_microservice() {
-    result=$(http_proxy="" curl http://localhost:9099/v1/audio/transcriptions -XPOST -d '{"byte_str": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA"}' -H 'Content-Type: application/json')
+    result=$(http_proxy="" curl http://localhost:9089/v1/audio/transcriptions -XPOST -d '{"byte_str": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA"}' -H 'Content-Type: application/json')
     if [[ $result == *"you"* ]]; then
         echo "Result correct."
     else
