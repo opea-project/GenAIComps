a9d1c47290bcfc76af02db2fd78662b611a76e4b
Xinyu Ye <xinyu.ye@intel.com>
2024-08-12 15:25:38 +0800

    added image2video microservice.
    Signed-off-by: Xinyu Ye <xinyu.ye@intel.com>

---
+++ comps/image2video/README.md
@@ -0,0 +1,67 @@
+# Image-to-Video Microservice
+
+Image-to-Video is a task that generate video in conditioning the provided image(s). This microservice supports image-to-video task by using Stable Video Diffusion (SVD) model.
+
+# 🚀1. Start Microservice with Python (Option 1)
+
+## 1.1 Install Requirements
+
+```bash
+pip install -r requirements.txt
+pip install -r svd/requirements.txt
+```
+
+## 1.2 Start SVD Service 
+
+```bash
+# Start SVD service
+cd svd/
+python svd_server.py
+```
+
+## 1.3 Start Image-to-Video Microservice
+
+```bash
+cd ..
+# Start the OPEA Microservice
+python image2video.py
+```
+
+# 🚀2. Start Microservice with Docker (Option 2)
+
+## 2.1 Build Images
+
+### 2.1.1 SVD Server Image
+
+```bash
+cd ../..
+docker build -t opea/svd:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/image2video/svd/Dockerfile .
+```
+
+### 2.1.2 Image-to-Video Service Image
+
+```bash
+docker build -t opea/image2video:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/image2video/Dockerfile .
+```
+
+## 2.2 Start SVD and Image-to-Video Service
+
+### 2.2.1 Start SVD server
+
+```bash
+docker run --ipc=host -p 9368:9368 -e http_proxy=$http_proxy -e https_proxy=$https_proxy opea/svd:latest
+```
+
+### 2.2.2 Start Image-to-Video service
+
+```bash
+ip_address=$(hostname -I | awk '{print $1}')
+
+docker run -p 9369:9369 --ipc=host -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e SVD_ENDPOINT=http://$ip_address:9368 opea/image2video:latest
+```
+
+### 2.2.3 Test
+
+```bash
+http_proxy="" curl http://localhost:9369/v1/image2video -XPOST -d '{"images_path":[{"image_path":"https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/svd/rocket.png"}]}' -H 'Content-Type: application/json'
+```
---
+++ comps/image2video/svd/svd_server.py
@@ -0,0 +1,64 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""Stand-alone Stable Video Diffusion FastAPI Server."""
+
+import argparse
+import os
+import time
+
+import torch
+import uvicorn
+from diffusers import StableVideoDiffusionPipeline
+from diffusers.utils import load_image, export_to_video
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, Response
+
+
+app = FastAPI()
+
+
+@app.get("/health")
+async def health() -> Response:
+    """Health check."""
+    return Response(status_code=200)
+
+
+@app.post("/generate")
+async def generate(request: Request) -> Response:
+    print("SVD generation begin.")
+    request_dict = await request.json()
+    images_path = request_dict.pop("images_path")
+
+    start = time.time()
+    images = [load_image(img) for img in images_path]
+    images = [image.resize((1024, 576)) for image in images]
+
+    generator = torch.manual_seed(args.seed)
+    frames = pipe(images, decode_chunk_size=8, generator=generator).frames[0]
+    video_path = os.path.join(os.getcwd(), args.video_path)
+    export_to_video(frames, video_path, fps=7)
+    end = time.time()
+    print(f"SVD video output in {video_path}, time = {end-start}s")
+    return JSONResponse({"video_path": video_path})
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=9368)
+    parser.add_argument("--model_name_or_path", type=str, default="stabilityai/stable-video-diffusion-img2vid-xt")
+    parser.add_argument("--video_path", type=str, default="generated.mp4")
+    parser.add_argument("--seed", type=int, default=42)
+
+    args = parser.parse_args()
+    pipe = StableVideoDiffusionPipeline.from_pretrained(
+        args.model_name_or_path
+    )
+    print("Stable Video Diffusion model initialized.")
+
+    uvicorn.run(
+        app,
+        host=args.host,
+        port=args.port,
+        log_level="debug",
+    )
