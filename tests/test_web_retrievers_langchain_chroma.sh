aab60ce0a31692ec41f9b2e2e6628f1366b1f240
zepan <ze.pan@intel.com>
2024-08-12 22:57:58 +0800

    Change validate log.
    Signed-off-by: zepan <ze.pan@intel.com>

--- tests/test_web_retrievers_langchain_chroma.sh
+++ tests/test_web_retrievers_langchain_chroma.sh
@@ -31,7 +31,7 @@ function validate_microservice() {
     retriever_port=5019
     export PATH="${HOME}/miniforge3/bin:$PATH"
     test_embedding=$(python -c "import random; embedding = [random.uniform(-1, 1) for _ in range(768)]; print(embedding)")
-    result=$(http_proxy='' curl -w "%{http_code}" http://${ip_address}:$retriever_port/v1/web_retrieval \
+    result=$(http_proxy='' curl http://${ip_address}:$retriever_port/v1/web_retrieval \
         -X POST \
         -d "{\"text\":\"What is OPEA?\",\"embedding\":${test_embedding}}" \
         -H 'Content-Type: application/json')
