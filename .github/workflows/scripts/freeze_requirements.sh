648d088d996373a100bdda9d3298a897918c5bf9
Sun, Xuehao <xuehao.sun@intel.com>
2024-08-14 14:46:22 +0800

    fix requirements reg
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .github/workflows/scripts/freeze_requirements.sh
+++ .github/workflows/scripts/freeze_requirements.sh
@@ -6,21 +6,25 @@
 function freeze() {
     local file=$1
     local folder=$(dirname "$file")
+    local keep_origin_packages="true"
     pip-compile \
         --no-upgrade \
-        --output-file \
         --no-annotate \
         --no-header \
-        "$folder/freeze.txt" "$file"
+        --output-file "$folder/freeze.txt" \
+        "$file"
     if [[ -e "$folder/freeze.txt" ]]; then
         if [[ "$keep_origin_packages" == "true" ]]; then
             packages1=$(cut -d'=' -f1 "$file" | sort)
             packages2=$(cut -d'=' -f1 "$folder/freeze.txt" | sort)
-            common_packages=$(comm -12 <(echo "$packages1") <(echo "$packages2"))
+            common_packages=$(comm -12 <(echo "$packages2" | tr '[:upper:]' '[:lower:]' | sed 's/[-_]/-/g') <(echo "$packages1" | tr '[:upper:]' '[:lower:]' | sed 's/[-_]/-/g'))
+
             rm "$file"
             while IFS= read -r line; do
                 package=$(echo "$line" | cut -d'=' -f1)
-                if echo "$common_packages" | grep -q "^$package$"; then
+                package_transformed=$(echo "$package" | tr '[:upper:]' '[:lower:]' | sed 's/[_-]/-/g')
+                pattern=$(echo "$package_transformed" | sed 's/\[/\\\[/g; s/\]/\\\]/g')
+                if echo "$common_packages" | grep -q "^$pattern$"; then
                     echo "$line" >>"$file"
                 fi
             done <"$folder/freeze.txt"
@@ -32,16 +36,14 @@ function freeze() {
     fi
 }
 
-
 function check_branch_name() {
     branch_name=$(git branch --show-current)
     if [[ "$branch_name" == *"rc" ]]; then
         echo "$branch_name is release branch"
     else
-        exit 0
+        echo "$branch_name is not release branch" && exit 0
     fi
 }
-
 
 function main() {
     check_branch_name
