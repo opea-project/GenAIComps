75c4a00fb591e45cd42795fd4461a5e1825b3691
test <test@intel.com>
2024-08-21 01:10:00 +0000

    update finetuning api with openai format.

--- comps/finetuning/finetuning_service.py
+++ comps/finetuning/finetuning_service.py
@@ -1,8 +1,7 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import uvicorn
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import BackgroundTasks
 
 from comps.finetuning.handlers import (
     handle_cancel_finetuning_job,
@@ -10,32 +9,43 @@ from comps.finetuning.handlers import (
     handle_list_finetuning_jobs,
     handle_retrieve_finetuning_job,
 )
-from comps.finetuning.models import FineTuningJob, FineTuningJobList, FineTuningJobsRequest
+from comps import opea_microservices, register_microservice
+from comps.cores.proto.api_protocol import FineTuningJobIDRequest, FineTuningJobsRequest
 
-app = FastAPI()
 
-
-@app.post("/v1/fine_tuning/jobs", response_model=FineTuningJob)
+@register_microservice(name="opea_service@finetuning", endpoint="/v1/fine_tuning/jobs", host="0.0.0.0", port=8001)
 def create_finetuning_jobs(request: FineTuningJobsRequest, background_tasks: BackgroundTasks):
     return handle_create_finetuning_jobs(request, background_tasks)
 
 
-@app.get("/v1/fine_tuning/jobs", response_model=FineTuningJobList)
+@register_microservice(
+    name="opea_service@finetuning", endpoint="/v1/fine_tuning/jobs", host="0.0.0.0", port=8001, methods=["GET"]
+)
 def list_finetuning_jobs():
     return handle_list_finetuning_jobs()
 
 
-@app.get("/v1/fine_tuning/jobs/{fine_tuning_job_id}", response_model=FineTuningJob)
-def retrieve_finetuning_job(fine_tuning_job_id):
-    job = handle_retrieve_finetuning_job(fine_tuning_job_id)
+@register_microservice(
+    name="opea_service@finetuning",
+    endpoint="/v1/fine_tuning/jobs/retrieve",
+    host="0.0.0.0",
+    port=8001
+)
+def retrieve_finetuning_job(request: FineTuningJobIDRequest):
+    job = handle_retrieve_finetuning_job(request)
     return job
 
 
-@app.post("/v1/fine_tuning/jobs/{fine_tuning_job_id}/cancel", response_model=FineTuningJob)
-def cancel_finetuning_job(fine_tuning_job_id):
-    job = handle_cancel_finetuning_job(fine_tuning_job_id)
+@register_microservice(
+    name="opea_service@finetuning",
+    endpoint="/v1/fine_tuning/jobs/cancel",
+    host="0.0.0.0",
+    port=8001
+)
+def cancel_finetuning_job(request: FineTuningJobIDRequest):
+    job = handle_cancel_finetuning_job(request)
     return job
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    opea_microservices["opea_service@finetuning"].start()
