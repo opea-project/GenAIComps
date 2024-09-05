030120ed38f7592df968977bf709faad3833ec52
Tiep Le <tiep.le@intel.com>
2024-09-05 08:22:37 +0000

    update test mmrag qna with videos
    Signed-off-by: Tiep Le <tiep.le@intel.com>

--- tests/cores/mega/test_multimodalrag_qna_with_videos_gateway.py
+++ tests/cores/mega/test_multimodalrag_qna_with_videos_gateway.py
@@ -3,9 +3,10 @@
 
 import json
 import unittest
-
-from comps import Gateway, ServiceOrchestrator, TextDoc, opea_microservices, register_microservice
+from typing import Union
+from comps import ServiceOrchestrator, TextDoc, opea_microservices, register_microservice
 from comps import (
+        LVMDoc,
         MultimodalDoc, 
         EmbedDoc, 
         EmbedMultimodalDoc, 
@@ -45,11 +46,16 @@ async def mm_retriever_add(request: EmbedMultimodalDoc) -> SearchedMultimodalDoc
     return res
 
 @register_microservice(name="lvm", host="0.0.0.0", port=8085, endpoint="/v1/lvm")
-async def lvm_add(request: LVMSearchedMultimodalDoc) -> TextDoc:
+async def lvm_add(request: Union[LVMDoc, LVMSearchedMultimodalDoc]) -> TextDoc:
     req = request.model_dump_json()
     req_dict = json.loads(req)
     print('req_dict_lvm', req_dict)
-    text = req_dict["initial_query"]
+    if isinstance(request, LVMSearchedMultimodalDoc):
+        print("request is the output of multimodal retriever")
+        text = req_dict["initial_query"]
+    else:
+        print("request is from user.")
+        text = req_dict["prompt"]
     text += "opea project!"
     res = {}
     res['text'] = text
@@ -57,34 +63,42 @@ async def lvm_add(request: LVMSearchedMultimodalDoc) -> TextDoc:
 
 
 class TestServiceOrchestrator(unittest.IsolatedAsyncioTestCase):
-    def setUp(self):
-        self.mm_embedding = opea_microservices["mm_embedding"]
-        self.mm_retriever = opea_microservices["mm_retriever"]
-        self.lvm = opea_microservices["lvm"]
-        self.mm_embedding.start()
-        self.mm_retriever.start()
-        self.lvm.start()
+    @classmethod
+    def setUpClass(cls):
+        cls.mm_embedding = opea_microservices["mm_embedding"]
+        cls.mm_retriever = opea_microservices["mm_retriever"]
+        cls.lvm = opea_microservices["lvm"]
+        cls.mm_embedding.start()
+        cls.mm_retriever.start()
+        cls.lvm.start()
 
-        self.service_builder = ServiceOrchestrator()
+        cls.service_builder = ServiceOrchestrator()
 
-        self.service_builder.add(opea_microservices["mm_embedding"]).add(opea_microservices["mm_retriever"]).add(opea_microservices["lvm"])
-        self.service_builder.flow_to(self.mm_embedding, self.mm_retriever)
-        self.service_builder.flow_to(self.mm_retriever, self.lvm)
+        cls.service_builder.add(opea_microservices["mm_embedding"]).add(opea_microservices["mm_retriever"]).add(opea_microservices["lvm"])
+        cls.service_builder.flow_to(cls.mm_embedding, cls.mm_retriever)
+        cls.service_builder.flow_to(cls.mm_retriever, cls.lvm)
 
-        self.follow_up_query_service_builder = ServiceOrchestrator()
-        self.follow_up_query_service_builder.add(self.lvm)
+        cls.follow_up_query_service_builder = ServiceOrchestrator()
+        cls.follow_up_query_service_builder.add(cls.lvm)
 
-        self.gateway = MultimodalRAGQnAWithVideosGateway(self.service_builder, self.follow_up_query_service_builder, port=9898)
+        cls.gateway = MultimodalRAGQnAWithVideosGateway(cls.service_builder, cls.follow_up_query_service_builder, port=9898)
 
-    def tearDown(self):
-        self.mm_embedding.stop()
-        self.mm_retriever.stop()
-        self.lvm.stop()
-        self.gateway.stop()
+    @classmethod
+    def tearDownClass(cls):
+        cls.mm_embedding.stop()
+        cls.mm_retriever.stop()
+        cls.lvm.stop()
+        cls.gateway.stop()
 
-    async def test_schedule(self):
+    async def test_service_builder_schedule(self):
         result_dict, _ = await self.service_builder.schedule(initial_inputs={"text": "hello, "})
         self.assertEqual(result_dict[self.lvm.name]["text"], "hello, opea project!")
+
+    async def test_follow_up_query_service_builder_schedule(self):
+        result_dict, _ = await self.follow_up_query_service_builder.schedule(initial_inputs={"prompt": "chao, ", "image" : "some image"})
+        print(result_dict)
+        self.assertEqual(result_dict[self.lvm.name]["text"], "chao, opea project!")
+        
 
 
 if __name__ == "__main__":
