fef7f4dfaf0c49e41c9c917694e7d6ccfcc34da7
letonghan <letong.han@intel.com>
2024-09-05 18:02:39 +0800

    change port of intent detection
    Signed-off-by: letonghan <letong.han@intel.com>

--- tests/test_intent_detection_langchain.sh
+++ tests/test_intent_detection_langchain.sh
@@ -12,21 +12,21 @@ function build_docker_images() {
 }
 
 function start_service() {
-    tgi_endpoint=5004
+    tgi_endpoint=5044
     # Remember to set HF_TOKEN before invoking this test!
     export HUGGINGFACEHUB_API_TOKEN=${HF_TOKEN}
     model=Intel/neural-chat-7b-v3-3
     docker run -d --name="test-comps-intent-tgi-endpoint" -p $tgi_endpoint:80 -v ./data:/data --shm-size 1g ghcr.io/huggingface/text-generation-inference:1.4 --model-id $model
 
     export TGI_LLM_ENDPOINT="http://${ip_address}:${tgi_endpoint}"
-    tei_service_port=5005
+    tei_service_port=5043
     unset http_proxy
     docker run -d --name="test-comps-intent-tei-server" -p ${tei_service_port}:9000 --ipc=host -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e TGI_LLM_ENDPOINT=$TGI_LLM_ENDPOINT -e HUGGINGFACEHUB_API_TOKEN=$HUGGINGFACEHUB_API_TOKEN opea/llm-tgi:latest
     sleep 3m
 }
 
 function validate_microservice() {
-    tei_service_port=5005
+    tei_service_port=5043
     http_proxy="" curl http://${ip_address}:${tei_service_port}/v1/chat/intent\
         -X POST \
         -d '{"query":"What is Deep Learning?","max_new_tokens":10,"top_k":1,"temperature":0.001,"streaming":false}' \
