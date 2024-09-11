36fea0979e430026bc0d869be383b5d14eeec87d
Xinyao Wang <xinyao.wang@intel.com>
2024-09-11 13:29:46 +0800

    add llamaindex for vllm
    Signed-off-by: Xinyao Wang <xinyao.wang@intel.com>

--- comps/llms/text-generation/vllm/llama_index/llm.py
+++ comps/llms/text-generation/vllm/llama_index/llm.py
@@ -4,7 +4,7 @@
 import os
 
 from fastapi.responses import StreamingResponse
-from langchain_community.llms import VLLMOpenAI
+from llama_index.llms.openai_like import OpenAILike
 
 from comps import (
     CustomLogger,
@@ -16,7 +16,7 @@ from comps import (
     register_microservice,
 )
 
-logger = CustomLogger("llm_vllm")
+logger = CustomLogger("llm_vllm_llamaindex")
 logflag = os.getenv("LOGFLAG", False)
 
 
@@ -33,7 +33,7 @@ def post_process_text(text: str):
 
 
 @register_microservice(
-    name="opea_service@llm_vllm",
+    name="opea_service@llm_vllm_llama_index",
     service_type=ServiceType.LLM,
     endpoint="/v1/chat/completions",
     host="0.0.0.0",
@@ -44,11 +44,11 @@ def llm_generate(input: LLMParamsDoc):
         logger.info(input)
     llm_endpoint = os.getenv("vLLM_ENDPOINT", "http://localhost:8008")
     model_name = os.getenv("LLM_MODEL", "meta-llama/Meta-Llama-3-8B-Instruct")
-    llm = VLLMOpenAI(
-        openai_api_key="EMPTY",
-        openai_api_base=llm_endpoint + "/v1",
+    llm = OpenAILike(
+        api_key="fake",
+        api_base=llm_endpoint + "/v1",
         max_tokens=input.max_new_tokens,
-        model_name=model_name,
+        model=model_name,
         top_p=input.top_p,
         temperature=input.temperature,
         streaming=input.streaming,
@@ -58,7 +58,7 @@ def llm_generate(input: LLMParamsDoc):
 
         def stream_generator():
             chat_response = ""
-            for text in llm.stream(input.query):
+            for text in llm.stream_complete(input.query):
                 chat_response += text
                 chunk_repr = repr(text.encode("utf-8"))
                 yield f"data: {chunk_repr}\n\n"
@@ -68,11 +68,11 @@ def llm_generate(input: LLMParamsDoc):
 
         return StreamingResponse(stream_generator(), media_type="text/event-stream")
     else:
-        response = llm.invoke(input.query)
+        response = llm.complete(input.query)
         if logflag:
             logger.info(response)
         return GeneratedDoc(text=response, prompt=input.query)
 
 
 if __name__ == "__main__":
-    opea_microservices["opea_service@llm_vllm"].start()
+    opea_microservices["opea_service@llm_vllm_llama_index"].start()
