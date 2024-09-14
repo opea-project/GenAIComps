a5309cc756bc66c25eac1a57224aa1ddc4b772ae
XuhuiRen <44249229+XuhuiRen@users.noreply.github.com>
2024-09-14 20:23:04 +0800

    Update config.py

--- comps/retrievers/neo4j/langchain/config.py
+++ comps/retrievers/neo4j/langchain/config.py
@@ -4,7 +4,7 @@
 import os
 
 # Neo4J configuration
-NEO4J_URL = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+NEO4J_URL = os.getenv("NEO4J_URI", "bolt://localhost:7688")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "test")
 
