8ae56eb466cec815ddaa41d4d127dc6e016a4938
Spycsh <sihan.chen@intel.com>
2024-10-14 01:04:29 -0700

    revert no-padding ones

--- comps/embeddings/tei/langchain/local_embedding_reranking.py
+++ comps/embeddings/tei/langchain/local_embedding_reranking.py
@@ -95,14 +95,14 @@ async def static_batching_infer(service_type: Enum, batch: list[dict]):
         logger.info(f"{service_type} {len(batch)} request inference begin >>>")
 
     if service_type == ServiceType.EMBEDDING:
-        # TODO PADDING TO (MAX_BS, MAX_LEN)
+        # TODO PADDING TO (MAX_BS, MAX_LEN) padding="max_length" max_length=128
         sentences = [req["request"].text for req in batch]
         with torch.no_grad():
-            encoded_input = embedding_tokenizer(sentences, padding="max_length", truncation=True, return_tensors="pt", max_length=128).to(
+            encoded_input = embedding_tokenizer(sentences, padding=True, truncation=True, return_tensors="pt",).to(
                 device="hpu"
             )
-            with torch.autocast("hpu", dtype=torch.bfloat16):
-                results = embedding_model.embed(encoded_input)
+            # with torch.autocast("hpu", dtype=torch.bfloat16):
+            results = embedding_model.embed(encoded_input)
 
         return [EmbedDoc(text=txt, embedding=embed_vector) for txt, embed_vector in zip(sentences, results)]
     elif service_type == ServiceType.RERANK:
@@ -115,11 +115,10 @@ async def static_batching_infer(service_type: Enum, batch: list[dict]):
                 pairs.append([req["request"].initial_query, req["request"].retrieved_docs[idx].text])
 
         with torch.no_grad():
-            inputs = reranking_tokenizer(pairs, padding="max_length", truncation=True, return_tensors="pt", max_length=128).to(
+            inputs = reranking_tokenizer(pairs, padding=True, truncation=True, return_tensors="pt",).to(
                 "hpu"
             )
-            with torch.autocast("hpu", dtype=torch.bfloat16):
-                scores = reranking_model.predict(inputs)
+            scores = reranking_model.predict(inputs)
 
         # reduce each query's best related doc
         final_results = []
