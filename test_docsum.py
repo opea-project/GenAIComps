89e76c75e2f6dff89eb931475d50032041b95e8a
Mustafa <mustafa.cetin@intel.com>
2024-10-17 17:15:48 -0700

    gateway
    Signed-off-by: Mustafa <mustafa.cetin@intel.com>

--- test_docsum.py
+++ test_docsum.py
@@ -9,11 +9,14 @@ from comps import DocSumGateway, MicroService, ServiceOrchestrator, ServiceType
 MEGA_SERVICE_HOST_IP = os.getenv("MEGA_SERVICE_HOST_IP", "0.0.0.0")
 MEGA_SERVICE_PORT = int(os.getenv("MEGA_SERVICE_PORT", 8888))
 
-V2A_SERVICE_HOST_IP = os.getenv("V2A_SERVICE_HOST_IP", "0.0.0.0")
-V2A_SERVICE_PORT = int(os.getenv("V2A_SERVICE_PORT", 7078))
+# V2A_SERVICE_HOST_IP = os.getenv("V2A_SERVICE_HOST_IP", "0.0.0.0")
+# V2A_SERVICE_PORT = int(os.getenv("V2A_SERVICE_PORT", 7078))
 
-ASR_SERVICE_HOST_IP = os.getenv("ASR_SERVICE_HOST_IP", "0.0.0.0")
-ASR_SERVICE_PORT = int(os.getenv("ASR_SERVICE_PORT", 9099))
+# ASR_SERVICE_HOST_IP = os.getenv("ASR_SERVICE_HOST_IP", "0.0.0.0")
+# ASR_SERVICE_PORT = int(os.getenv("ASR_SERVICE_PORT", 9099))
+
+DATA_SERVICE_HOST_IP = os.getenv("DATA_SERVICE_HOST_IP", "0.0.0.0")
+DATA_SERVICE_PORT = int(os.getenv("DATA_SERVICE_PORT", 7078))
 
 LLM_SERVICE_HOST_IP = os.getenv("LLM_SERVICE_HOST_IP", "0.0.0.0")
 LLM_SERVICE_PORT = int(os.getenv("LLM_SERVICE_PORT", 9000))
@@ -44,6 +47,15 @@ class DocSumService:
         #     use_remote_service=True,
         #     service_type=ServiceType.ASR,
         # )
+        
+        data = MicroService(
+            name="data",
+            host=DATA_SERVICE_HOST_IP,
+            port=DATA_SERVICE_PORT,
+            endpoint="/v1/docsum/dataprep",
+            use_remote_service=True,
+            service_type=ServiceType.DATAPREP,
+        )
                 
         llm = MicroService(
             name="llm",
@@ -54,12 +66,12 @@ class DocSumService:
             service_type=ServiceType.LLM,
         )
         
-        self.megaservice.add(llm)
-        # self.megaservice.add(v2a).add(asr).add(llm)
+        # self.megaservice.add(llm)
+        self.megaservice.add(data).add(llm)
+        self.megaservice.flow_to(data, llm)
         
         # self.megaservice.flow_to(v2a, asr)
-        # self.megaservice.flow_to(asr, llm)
-        
+                
         self.gateway = DocSumGateway(megaservice=self.megaservice, host="0.0.0.0", port=self.port)
 
 
