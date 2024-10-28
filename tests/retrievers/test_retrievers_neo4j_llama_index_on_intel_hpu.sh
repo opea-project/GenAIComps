be2b3e5cbee7332964970c1a47e1b551e1a32066
Rita Brugarolas <rita.brugarolas.brufau@intel.com>
2024-10-28 21:47:26 +0000

    set 4 hpu for 70B model in ci test
    Signed-off-by: Rita Brugarolas <rita.brugarolas.brufau@intel.com>

--- tests/retrievers/test_retrievers_neo4j_llama_index_on_intel_hpu.sh
+++ tests/retrievers/test_retrievers_neo4j_llama_index_on_intel_hpu.sh
@@ -43,7 +43,7 @@ function start_service() {
     # tgi gaudi endpoint
     #model="meta-llama/Meta-Llama-3-8B-Instruct"
     model="meta-llama/Llama-2-70b-hf"
-    docker run -d --name="test-comps-retrievers-neo4j-llama-index-tgi" -p 6005:80 -v ./data:/data --runtime=habana -e PT_HPU_ENABLE_LAZY_COLLECTIVES=true -e HABANA_VISIBLE_DEVICES=all \
+    docker run -d --name="test-comps-retrievers-neo4j-llama-index-tgi" -p 6005:80 -v ./data:/data --runtime=habana -e PT_HPU_ENABLE_LAZY_COLLECTIVES=true -e HABANA_VISIBLE_DEVICES=0,1,2,3 \
         -e OMPI_MCA_btl_vader_single_copy_mechanism=none -e HF_TOKEN=$HF_TOKEN -e ENABLE_HPU_GRAPH=true -e LIMIT_HPU_GRAPH=true \
         -e USE_FLASH_ATTENTION=true -e FLASH_ATTENTION_RECOMPUTE=true --cap-add=sys_nice -e no_proxy=$no_proxy -e http_proxy=$http_proxy -e https_proxy=$https_proxy \
         --ipc=host --pull always ghcr.io/huggingface/tgi-gaudi:2.0.5 --model-id $model --max-input-tokens 1024 --max-total-tokens 3000
