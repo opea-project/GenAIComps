5ef283f6190fe00562b4addfe463a9a3c4e10492
sgurunat <gurunath.s@intel.com>
2024-10-29 09:59:07 +0000

    Update reranking_tei to support authentication for tgi endpoints. Uses get_access_token from utils

--- comps/reranks/tei/reranking_tei.py
+++ comps/reranks/tei/reranking_tei.py
@@ -24,10 +24,16 @@ from comps.cores.proto.api_protocol import (
     RerankingResponse,
     RerankingResponseData,
 )
+from comps.cores.mega.utils import get_access_token
+
 
 logger = CustomLogger("reranking_tei")
 logflag = os.getenv("LOGFLAG", False)
 
+# Environment variables
+TOKEN_URL = os.getenv("TOKEN_URL")
+CLIENTID = os.getenv("CLIENTID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 @register_microservice(
     name="opea_service@reranking_tei",
@@ -46,6 +52,7 @@ async def reranking(
         logger.info(input)
     start = time.time()
     reranking_results = []
+    access_token = get_access_token(TOKEN_URL, CLIENTID, CLIENT_SECRET) if TOKEN_URL and CLIENTID and CLIENT_SECRET else None
     if input.retrieved_docs:
         docs = [doc.text for doc in input.retrieved_docs]
         url = tei_reranking_endpoint + "/rerank"
@@ -56,6 +63,8 @@ async def reranking(
             query = input.input
         data = {"query": query, "texts": docs}
         headers = {"Content-Type": "application/json"}
+        if access_token:
+            headers = {"Content-Type": "application/json", "Authorization": f"Bearer {access_token}"}
         async with aiohttp.ClientSession() as session:
             async with session.post(url, data=json.dumps(data), headers=headers) as response:
                 response_data = await response.json()
