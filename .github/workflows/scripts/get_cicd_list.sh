68189a06c6f1e4230d17c61f9f68832c3fa31ab7
ZePan110 <ze.pan@intel.com>
2024-11-07 21:38:55 +0800

    Combine CI/CD docker compose yaml files.
    Signed-off-by: ZePan110 <ze.pan@intel.com>

---
+++ .github/workflows/scripts/get_cicd_list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+WORKPATH=$PWD
+# echo "mode is: "$1 "compose file is: "$2
+all_ci_yaml=$(find /home/zepan/OPEA/GenAIExamples -name "build.yaml")
+all_ci_lists=$(grep '^[[:space:]]\{2\}[a-zA-Z0-9_-]\+:' $all_ci_yaml | awk -F: '{print$2}' | sed 's/^[[:space:]]\+//' | tr '\n' ',' | sed 's/,$//')
+
+all_lists=$(grep '^[[:space:]]\{2\}[a-zA-Z0-9_-]\+:' $2 | sed 's/^[[:space:]]\{2\}\([a-zA-Z0-9_-]\+\):.*/\1/' | tr '\n' ',' | sed 's/,$//')
+cd_lists=$(echo "$all_lists" | tr ',' '\n' | grep -v -F -x -f <(echo "$all_ci_lists" | tr ',' '\n') | tr '\n' ',' | sed 's/,$//')
+ci_lists=$(echo "$all_lists" | tr ',' '\n' | grep -F -x -f <(echo "$all_ci_lists" | tr ',' '\n') | tr '\n' ',' | sed 's/,$//')
+if [[ "$1" == "CI" ]]; then
+    echo "$ci_lists"
+elif [[ "$1" == "CD" ]]; then
+    echo "$cd_lists"
+fi
