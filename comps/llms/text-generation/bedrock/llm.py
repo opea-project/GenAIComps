ffb4fc9817f97a95ab4710fdad8ca91108ce0526
root <root@ip-172-31-66-164.ec2.internal>
2024-11-18 14:19:34 +0000

    readme

--- comps/llms/text-generation/bedrock/llm.py
+++ comps/llms/text-generation/bedrock/llm.py
@@ -1,9 +1,13 @@
-import os
-import boto3
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
 import json
-from fastapi.responses import StreamingResponse
-from comps import CustomLogger, GeneratedDoc, LLMParamsDoc, ServiceType, opea_microservices, register_microservice
+import os
 from typing import Union
+
+import boto3
+from fastapi.responses import StreamingResponse
+
 from comps import (
     CustomLogger,
     GeneratedDoc,
@@ -17,8 +21,7 @@ from comps import (
 )
 from comps.cores.proto.api_protocol import ChatCompletionRequest
 
-bedrock_runtime = boto3.client(
-    service_name="bedrock-runtime", region_name="us-east-1")
+bedrock_runtime = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
 
 logger = CustomLogger("llm_bedrock_anthropic")
 logflag = os.getenv("LOGFLAG", True)
@@ -35,55 +38,46 @@ def llm_generate(input: Union[LLMParamsDoc, ChatCompletionRequest, SearchedDoc])
     if logflag:
         logger.info(input)
     print(input.messages)
-    content = input.messages[0]['content']
+    content = input.messages[0]["content"]
     print(content)
     kwargs = {
         "modelId": "us.anthropic.claude-3-5-haiku-20241022-v1:0",
         "contentType": "application/json",
         "accept": "application/json",
-        "body": json.dumps({
-            "anthropic_version": "bedrock-2023-05-31",
-            "max_tokens": 1000,
-            "messages": [
-                {
-                    "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": content
-                        }
-                    ]
-                }
-            ]
-        })
+        "body": json.dumps(
+            {
+                "anthropic_version": "bedrock-2023-05-31",
+                "max_tokens": 1000,
+                "messages": [{"role": "user", "content": [{"type": "text", "text": content}]}],
+            }
+        ),
     }
 
     if input.stream:
+
         def stream_generator():
             response = bedrock_runtime.invoke_model_with_response_stream(**kwargs)
-            for event in response.get('body'):
-                chunk = json.loads(event['chunk']['bytes'])    
+            for event in response.get("body"):
+                chunk = json.loads(event["chunk"]["bytes"])
                 print(chunk)
-                if chunk['type'] == 'content_block_delta':
-                    text_chunk = chunk['delta']['text']
-                    chunk_repr = repr(text_chunk.encode('utf-8'))
+                if chunk["type"] == "content_block_delta":
+                    text_chunk = chunk["delta"]["text"]
+                    chunk_repr = repr(text_chunk.encode("utf-8"))
                     if logflag:
-                        logger.info(
-                            f"[llm - chat_stream] chunk:{chunk_repr}")
+                        logger.info(f"[llm - chat_stream] chunk:{chunk_repr}")
                     yield f"data: {chunk_repr}\n\n"
-                yield "data: [DONE]\n\n"
-        
+            yield "data: [DONE]\n\n"
+
         return StreamingResponse(stream_generator(), media_type="text/event-stream")
     else:
         response = bedrock_runtime.invoke_model(**kwargs)
-        response_body = json.loads(response.get('body').read())
-        generated_text = response_body.get('content')[0].get('text')
+        response_body = json.loads(response.get("body").read())
+        generated_text = response_body.get("content")[0].get("text")
         print(generated_text)
         if logflag:
             logger.info(generated_text)
-        return GeneratedDoc(text=generated_text,  prompt=content)
+        return GeneratedDoc(text=generated_text, prompt=content)
 
 
 if __name__ == "__main__":
     opea_microservices["opea_service@llm_bedrock_anthropic"].start()
-
