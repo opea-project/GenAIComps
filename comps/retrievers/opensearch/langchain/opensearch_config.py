4db934572603a743a52ae195ccc55db6e1c98ad3
Cameron Morin <cammorin@amazon.com>
2024-11-18 23:44:14 +0000

    Address PR comments
    Signed-off-by: Cameron Morin <cammorin@amazon.com>

--- comps/retrievers/opensearch/langchain/opensearch_config.py
+++ comps/retrievers/opensearch/langchain/opensearch_config.py
@@ -46,7 +46,7 @@ EMBED_MODEL = os.getenv("EMBED_MODEL", "BAAI/bge-base-en-v1.5")
 # OpenSearch Connection Information
 OPENSEARCH_HOST = os.getenv("OPENSEARCH_HOST", "localhost")
 OPENSEARCH_PORT = int(os.getenv("OPENSEARCH_PORT", 9200))
-OPENSEARCH_INITIAL_ADMIN_PASSWORD = os.getenv("OPENSEARCH_INITIAL_ADMIN_PASSWORD", "strongOpea0!")
+OPENSEARCH_INITIAL_ADMIN_PASSWORD = os.getenv("OPENSEARCH_INITIAL_ADMIN_PASSWORD", "StRoNgOpEa0)")
 
 
 def format_opensearch_conn_from_env():
