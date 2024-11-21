6b9a010991a4b2adf52c9f60072c5af37125a320
Cameron Morin <cammorin@amazon.com>
2024-11-21 00:39:56 +0000

    Finalize dataprep script (working!)
    Signed-off-by: Cameron Morin <cammorin@amazon.com>

--- tests/dataprep/test_dataprep_opensearch_langchain.sh
+++ tests/dataprep/test_dataprep_opensearch_langchain.sh
@@ -6,9 +6,9 @@ set -x
 
 WORKPATH=$(dirname "$PWD")
 LOG_PATH="$WORKPATH/tests"
-# ip_address=$(hostname -I | awk '{print $1}')
-ip_address="localhost"
+ip_address=$(hostname -I | awk '{print $1}')
 dataprep_service_port="6007"
+OPENSEARCH_INITIAL_ADMIN_PASSWORD="StRoNgOpEa0)"
 
 function build_docker_images() {
     cd $WORKPATH
@@ -24,7 +24,6 @@ function build_docker_images() {
 
 function start_service() {
     # Start OpenSearch vector db container
-    OPENSEARCH_INITIAL_ADMIN_PASSWORD="StRoNgOpEa0)"
     docker run -d \
         --name test-comps-dataprep-opensearch-langchain \
         -e cluster.name=opensearch-cluster \
@@ -41,7 +40,8 @@ function start_service() {
         opensearchproject/opensearch:latest
     
     # Start OpenSearch dataprep container
-    OPENSEARCH_URL="http://localhost:9200"
+    OPENSEARCH_URL="http://${ip_address}:9200"
+    echo $(OPENSEARCH_URL)
     INDEX_NAME="file-index"
     docker run -d \
         --name test-comps-dataprep-opensearch-langchain-server \
@@ -62,7 +62,7 @@ function validate_microservice() {
     # test /v1/dataprep upload file
     URL="http://${ip_address}:$dataprep_service_port/v1/dataprep"
     echo "Deep learning is a subset of machine learning that utilizes neural networks with multiple layers to analyze various levels of abstract data representations. It enables computers to identify patterns and make decisions with minimal human intervention by learning from large amounts of data." > $LOG_PATH/dataprep_file.txt
-    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -F 'files=@./dataprep_file.txt' -H 'Content-Type: multipart/form-data' "$URL")
+    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -F 'files=@./dataprep_file.txt' -H 'Content-Type: multipart/form-data' -k -u admin:$OPENSEARCH_INITIAL_ADMIN_PASSWORD "$URL")
     HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
     RESPONSE_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
     SERVICE_NAME="dataprep - upload - file"
@@ -82,9 +82,10 @@ function validate_microservice() {
         echo "[ $SERVICE_NAME ] Content is as expected."
     fi
 
+
     # test /v1/dataprep upload link
     URL="http://${ip_address}:$dataprep_service_port/v1/dataprep"
-    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -F 'link_list=["https://www.ces.tech/"]' "$URL")
+    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -F 'link_list=["https://www.ces.tech/"]' -k -u admin:$OPENSEARCH_INITIAL_ADMIN_PASSWORD "$URL")
     HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
     RESPONSE_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
     SERVICE_NAME="dataprep - upload - link"
@@ -107,7 +108,7 @@ function validate_microservice() {
 
     # test /v1/dataprep/get_file
     URL="http://${ip_address}:$dataprep_service_port/v1/dataprep/get_file"
-    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST "$URL")
+    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -k -u admin:$OPENSEARCH_INITIAL_ADMIN_PASSWORD "$URL")
     HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
     RESPONSE_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
     SERVICE_NAME="dataprep - get"
@@ -119,7 +120,7 @@ function validate_microservice() {
     else
         echo "[ $SERVICE_NAME ] HTTP status is 200. Checking content..."
     fi
-    if [[ "$RESPONSE_BODY" != *'{"name":'* ]]; then
+    if [[ "$RESPONSE_BODY" -ne "null" ]]; then
         echo "[ $SERVICE_NAME ] Content does not match the expected result: $RESPONSE_BODY"
         docker logs test-comps-dataprep-opensearch-langchain-server >> ${LOG_PATH}/dataprep_file.log
         exit 1
@@ -129,21 +130,21 @@ function validate_microservice() {
 
     # test /v1/dataprep/delete_file
     URL="http://${ip_address}:$dataprep_service_port/v1/dataprep/delete_file"
-    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -d '{"file_path": "dataprep_file.txt"}' -H 'Content-Type: application/json' "$URL")
+    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -d '{"file_path": "dataprep_file.txt"}' -H 'Content-Type: application/json' -k -u admin:$OPENSEARCH_INITIAL_ADMIN_PASSWORD "$URL")
     HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
     RESPONSE_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
     SERVICE_NAME="dataprep - del"
 
     # check response status
-    if [ "$HTTP_STATUS" -ne "200" ]; then
-        echo "[ $SERVICE_NAME ] HTTP status is not 200. Received status was $HTTP_STATUS"
+    if [ "$HTTP_STATUS" -ne "404" ]; then
+        echo "[ $SERVICE_NAME ] HTTP status is not 404. Received status was $HTTP_STATUS"
         docker logs test-comps-dataprep-opensearch-langchain-server >> ${LOG_PATH}/dataprep_del.log
         exit 1
     else
-        echo "[ $SERVICE_NAME ] HTTP status is 200. Checking content..."
+        echo "[ $SERVICE_NAME ] HTTP status is 404. Checking content..."
     fi
     # check response body
-    if [[ "$RESPONSE_BODY" != *'{"status":true}'* ]]; then
+    if [[ "$RESPONSE_BODY" != *'{"detail":"Single file deletion is not implemented yet"}'* ]]; then
         echo "[ $SERVICE_NAME ] Content does not match the expected result: $RESPONSE_BODY"
         docker logs test-comps-dataprep-opensearch-langchain-server >> ${LOG_PATH}/dataprep_del.log
         exit 1
@@ -171,3 +172,4 @@ function main() {
 }
 
 main
+
