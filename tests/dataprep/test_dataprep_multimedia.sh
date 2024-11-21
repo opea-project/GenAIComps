3cf4d52c6c3b5c2336a21dc8522ae6e9afa3986a
Mustafa <mustafa.cetin@intel.com>
2024-11-21 14:01:40 -0800

    initial commit with fix
    Signed-off-by: Mustafa <mustafa.cetin@intel.com>

--- tests/dataprep/test_dataprep_multimedia.sh
+++ tests/dataprep/test_dataprep_multimedia.sh
@@ -192,7 +192,7 @@ function validate_microservices() {
     # Video2Audio service
     validate_services \
         "${host_ip}:7078/v1/video2audio" \
-        "SUQzBAAAAAAAI1RTU0UAAAAPAAADTGF2ZjU4LjI5LjEwMAAAAAAAAAAAAAAA//tQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASW5mbwAAAA8AAAAIAAAN3wAtLS0tLS0tLS0tLS1LS0tLS0tLS0tLS0tpaWlpaWlpaWlpaWlph4eHh4eHh4eHh4eHpaWlpaWlpaWlpaWlpcPDw8PDw8PDw8PDw+Hh4eHh4eHh4eHh4eH///////////////8AAAAATGF2YzU4LjU0AAAAAAAAAAAAAAAAJAYwAAAAAAAADd9L18KaAAAAAAAAAAAAAAAAAAAAAP/7kGQAAAMhClSVMEACMOAabaCMAREA" \
+        "SUQzBAAAAAAAI1RTU0UAAAAPAAADTGF2ZjU4LjI5LjEwMAAAAAAAAAAAAAAA//tQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASW5mbwAAAA8AAAAIAAAN3wAtLS0tLS0tLS0tLS1LS0tLS0tLS0tLS0tpaWlpaWlpaWlpaWlph4eHh4eHh4eHh4eHpaWlpaWlpaWlpaWlpcPDw8PDw8PDw8PDw+Hh4eHh4eHh4eHh4eH///////////////8AAAAATGF2YzU4LjU0AAAAAAAAAAAAAAA" \
         "v2a" \
         "v2a-service" \
         "{\"byte_str\": \"$(input_data_for_test "video")\"}"
@@ -200,7 +200,7 @@ function validate_microservices() {
     # Docsum Data service - video
     validate_services \
         "${host_ip}:7079/v1/multimedia2text" \
-        '"query":"well"' \
+        '"query":"well' \
         "multimedia2text-service" \
         "multimedia2text" \
         "{\"video\": \"$(input_data_for_test "video")\"}"
@@ -208,7 +208,7 @@ function validate_microservices() {
     # Docsum Data service - audio
     validate_services \
         "${host_ip}:7079/v1/multimedia2text" \
-        '"query":"well"' \
+        '"query":"well' \
         "multimedia2text-service" \
         "multimedia2text" \
         "{\"audio\": \"$(input_data_for_test "audio")\"}"
