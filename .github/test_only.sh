046f7a7a51a07736b8a2e066f61ac0446e517412
Wenxin Zhang <wenxin.zhang@intel.com>
2024-11-29 09:44:53 +0800

    add dangerous cmd check
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

---
+++ .github/test_only.sh
@@ -0,0 +1,8 @@
+#This is for test only
+docker images
+docker stop $(docker ps -a -q) # this is the dangerous cmd
+docker stop # this is harmless
+docker ps -a # this is harmless
+docker ps -q # this is harmless
+sudo rm -fr  # this is the dangerous cmd
+rm -fr # this is harmless
