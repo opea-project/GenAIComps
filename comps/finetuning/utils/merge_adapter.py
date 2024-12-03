a0446eb0dffbe857f6483106009b59643545af3c
Yuan0320 <jinjie.yuan@intel.com>
2024-12-03 19:53:47 +0800

    fix: resolve ipex issue and remove useless code in merge_adapter.py
    Co-authored-by: J. Pablo Muñoz <pablo.munoz@intel.com>
    Signed-off-by: Yuan0320 <jinjie.yuan@intel.com>

--- comps/finetuning/utils/merge_adapter.py
+++ comps/finetuning/utils/merge_adapter.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import intel_extension_for_pytorch
 from peft import PeftModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
@@ -9,8 +10,6 @@ def main(base_model_path, adapter_model_path, output_path):
     base_model = AutoModelForCausalLM.from_pretrained(base_model_path, trust_remote_code=True)
     model = PeftModel.from_pretrained(base_model, adapter_model_path)
     model.eval()
-    for name, param in model.named_parameters():
-        param.requires_grad = False
     merged_model = model.merge_and_unload()
     merged_model.train(False)
     base_model.save_pretrained(output_path, state_dict=merged_model.state_dict())
