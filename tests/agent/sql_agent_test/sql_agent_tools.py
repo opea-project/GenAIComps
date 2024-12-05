a63d3bc4e1ab3f101b345ec1442c8a546e6090e6
minmin-intel <minmin.hou@intel.com>
2024-12-05 12:03:39 -0800

    add tag-bench test and google search tool
    Signed-off-by: minmin-intel <minmin.hou@intel.com>

---
+++ tests/agent/sql_agent_test/sql_agent_tools.py
@@ -0,0 +1,15 @@
+def search_web(query: str)->str:
+    '''Search the web for information not contained in databases.'''
+    from langchain_core.tools import Tool
+    from langchain_google_community import GoogleSearchAPIWrapper
+
+    search = GoogleSearchAPIWrapper()
+
+    tool = Tool(
+        name="google_search",
+        description="Search Google for recent results.",
+        func=search.run,
+    )
+
+    response = tool.run(query)
+    return response
--- tests/agent/sql_agent_test/test_sql_agent.sh
+++ tests/agent/sql_agent_test/test_sql_agent.sh
@@ -19,8 +19,7 @@ export agent_image="opea/agent-langchain:comps"
 export agent_container_name="test-comps-agent-endpoint"
 
 export ip_address=$(hostname -I | awk '{print $1}')
-tgi_port=8085
-tgi_volume=${HF_CACHE_DIR} #$WORKPATH/data
+
 vllm_port=8085
 vllm_volume=${HF_CACHE_DIR} #$WORKPATH/data
 export model=meta-llama/Meta-Llama-3.1-70B-Instruct
@@ -29,11 +28,16 @@ export LLM_MODEL_ID="meta-llama/Meta-Llama-3.1-70B-Instruct"
 export LLM_ENDPOINT_URL="http://${ip_address}:${tgi_port}"
 export temperature=0.01
 export max_new_tokens=4096
-export TOOLSET_PATH=$WORKPATH/comps/agent/langchain/tools/
+export TOOLSET_PATH=$WORKPATH/tests/agent/sql_agent_test/
 echo "TOOLSET_PATH=${TOOLSET_PATH}"
 export recursion_limit=15
 export db_name=california_schools
 export db_path=/home/user/TAG-Bench/dev_folder/dev_databases/${db_name}/${db_name}.sqlite
+
+# for using Google search API
+export GOOGLE_CSE_ID=${GOOGLE_CSE_ID}
+export GOOGLE_API_KEY=${GOOGLE_API_KEY}
+
 
 # download the test data
 function prepare_data() {
@@ -65,28 +69,6 @@ function build_docker_images() {
     fi
 }
 
-# launch tgi-gaudi
-function start_tgi_service() {
-    echo "token is ${HF_TOKEN}"
-
-    #multi cards
-    echo "start tgi gaudi service"
-    docker run -d --runtime=habana --name "test-comps-tgi-gaudi-service" -p $tgi_port:80 -v $tgi_volume:/data -e HF_TOKEN=$HF_TOKEN -e HABANA_VISIBLE_DEVICES=0,1,2,3 -e OMPI_MCA_btl_vader_single_copy_mechanism=none -e PT_HPU_ENABLE_LAZY_COLLECTIVES=true -e http_proxy=$http_proxy -e https_proxy=$https_proxy --cap-add=sys_nice --ipc=host ghcr.io/huggingface/tgi-gaudi:2.0.5 --model-id $model --max-input-tokens 8192 --max-total-tokens 16384 --sharded true --num-shard 4
-    sleep 5s
-    echo "Waiting tgi gaudi ready"
-    n=0
-    until [[ "$n" -ge 100 ]] || [[ $ready == true ]]; do
-        docker logs test-comps-tgi-gaudi-service &> ${LOG_PATH}/tgi-gaudi-service.log
-        n=$((n+1))
-        if grep -q Connected ${LOG_PATH}/tgi-gaudi-service.log; then
-            break
-        fi
-        sleep 5s
-    done
-    sleep 5s
-    echo "Service started successfully"
-}
-
 function build_vllm_docker_images() {
     echo "Building the vllm docker images"
     cd $WORKPATH
@@ -95,7 +77,7 @@ function build_vllm_docker_images() {
         git clone https://github.com/HabanaAI/vllm-fork.git
     fi
     cd ./vllm-fork
-    docker build -f Dockerfile.hpu -t opea/vllm-gaudi:comps --shm-size=128g . --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy
+    docker build --no-cache -f Dockerfile.hpu -t opea/vllm-gaudi:comps --shm-size=128g . --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy
     if [ $? -ne 0 ]; then
         echo "opea/vllm-gaudi:comps failed"
         exit 1
@@ -111,7 +93,7 @@ function start_vllm_service() {
     #single card
     echo "start vllm gaudi service"
     echo "**************model is $model**************"
-    docker run -d --runtime=habana --rm --name "test-comps-vllm-gaudi-service" -e HABANA_VISIBLE_DEVICES=0,1,2,3 -p $vllm_port:80 -v $vllm_volume:/data -e HF_TOKEN=$HF_TOKEN -e HF_HOME=/data -e OMPI_MCA_btl_vader_single_copy_mechanism=none -e PT_HPU_ENABLE_LAZY_COLLECTIVES=true -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e VLLM_SKIP_WARMUP=true --cap-add=sys_nice --ipc=host opea/vllm-gaudi:comps --model ${model} --host 0.0.0.0 --port 80 --block-size 128 --max-num-seqs  4096 --max-seq_len-to-capture 8192 --tensor-parallel-size 4
+    docker run -d --runtime=habana --rm --name "test-comps-vllm-gaudi-service" -e HABANA_VISIBLE_DEVICES=0,1,2,3 -p $vllm_port:80 -v $vllm_volume:/data -e HF_TOKEN=$HF_TOKEN -e HF_HOME=/data -e OMPI_MCA_btl_vader_single_copy_mechanism=none -e PT_HPU_ENABLE_LAZY_COLLECTIVES=true -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e VLLM_SKIP_WARMUP=true --cap-add=sys_nice --ipc=host opea/vllm-gaudi:comps --model ${model} --host 0.0.0.0 --port 80 --block-size 128 --max-seq_len-to-capture 16384 --tensor-parallel-size 4
     sleep 5s
     echo "Waiting vllm gaudi ready"
     n=0
@@ -146,11 +128,17 @@ function run_test() {
     python3 test.py --test-sql-agent
 }
 
+function run_benchmark() {
+    echo "Running benchmark..."
+    cd $WORKPATH/tests/agent/sql_agent_test
+    query_file=$$WORKDIR/TAG-Bench/query_by_db/query_california_schools.csv
+    outdir=$WORKDIR/sql_agent_output
+    outfile=california_school_agent_test_result.csv
+    python3 test_tag_bench.py --query_file
+}
+
 # echo "Building docker image...."
 # build_docker_images
-
-# echo "Lauching TGI-gaudi...."
-# start_tgi_service
 
 echo "Building vllm docker image...."
 build_vllm_docker_images
