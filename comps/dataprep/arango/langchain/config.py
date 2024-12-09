7b5612cd520ffa131a84588951096cf19099630b
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-12-09 09:57:28 -0500

    new: envs in `config`

--- comps/dataprep/arango/langchain/config.py
+++ comps/dataprep/arango/langchain/config.py
@@ -9,9 +9,13 @@ ARANGO_USERNAME = os.getenv("ARANGO_USERNAME", "root")
 ARANGO_PASSWORD = os.getenv("ARANGO_PASSWORD", "test")
 DB_NAME = os.getenv("DB_NAME", "_system")
 
-# LLM/Embedding endpoints
+# Text Generation Inference configuration
 TGI_LLM_ENDPOINT = os.getenv("TGI_LLM_ENDPOINT", "http://localhost:8080")
-TGI_LLM_ENDPOINT_NO_RAG = os.getenv("TGI_LLM_ENDPOINT_NO_RAG", "http://localhost:8081")
-TEI_EMBEDDING_ENDPOINT = os.getenv("TEI_ENDPOINT")
+
+# Text Embeddings Inference configuration
+TEI_EMBEDDING_ENDPOINT = os.getenv("TEI_EMBEDDING_ENDPOINT")
+HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 TEI_EMBED_MODEL = os.getenv("TEI_EMBED_MODEL", "BAAI/bge-base-en-v1.5")
+
+# OpenAI configuration (alternative to TGI & TEI)
 OPENAI_KEY = os.getenv("OPENAI_API_KEY")
