3828ff816910c5dc27c8a6ed2050c8ede098911b
Wang, Kai Lawrence <kai.lawrence.wang@intel.com>
2024-12-10 11:15:45 +0800

    Increase the llm max timeout in ci for fully warmup
    Signed-off-by: Wang, Kai Lawrence <kai.lawrence.wang@intel.com>

--- tests/llms/test_llms_text-generation_vllm_langchain_on_intel_hpu.sh
+++ tests/llms/test_llms_text-generation_vllm_langchain_on_intel_hpu.sh
@@ -62,7 +62,7 @@ function start_service() {
 
     # check whether vllm ray is fully ready
     n=0
-    until [[ "$n" -ge 120 ]] || [[ $ready == true ]]; do
+    until [[ "$n" -ge 160 ]] || [[ $ready == true ]]; do
         docker logs test-comps-vllm-service > ${WORKPATH}/tests/test-comps-vllm-service.log
         n=$((n+1))
         if grep -q throughput ${WORKPATH}/tests/test-comps-vllm-service.log; then
