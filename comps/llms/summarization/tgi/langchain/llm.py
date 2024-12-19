637cea8bb2cc05116e327513e2d1a403465455eb
Xinyao Wang <xinyao.wang@intel.com>
2024-12-19 15:03:43 +0800

    docsum refine mode promt update
    Signed-off-by: Xinyao Wang <xinyao.wang@intel.com>

--- comps/llms/summarization/tgi/langchain/llm.py
+++ comps/llms/summarization/tgi/langchain/llm.py
@@ -42,26 +42,43 @@ templ_zh = """请简要概括以下内容:
 概况:"""
 
 
-templ_refine_en = """\
-Your job is to produce a final summary.
-We have provided an existing summary up to a certain point: {existing_answer}
-We have the opportunity to refine the existing summary (only if needed) with some more context below.
-------------
-{text}
-------------
-Given the new context, refine the original summary.
-If the context isn't useful, return the original summary.\
+templ_refine_en = """Your job is to produce a final summary.
+We have provided an existing summary up to a certain point, then we will provide more context.
+You need to refine the existing summary (only if needed) with new context and generate a final summary.
+
+
+Existing Summary: 
+"{existing_answer}"
+
+
+
+New Context:
+"{text}"
+
+
+
+Final Summary:
+
 """
 
 templ_refine_zh = """\
 你的任务是生成一个最终摘要。
-我们已经提供了部分摘要：{existing_answer}
-如果有需要的话，可以通过以下更多上下文来完善现有摘要。
-------------
-{text}
-------------
-根据新上下文，完善原始摘要。
-如果上下文无用，则返回原始摘要。\
+我们已经处理好部分文本并生成初始摘要, 并提供了新的未处理文本
+你需要根据新提供的文本，结合初始摘要，生成一个最终摘要。
+
+
+初始摘要: 
+"{existing_answer}"
+
+
+
+新的文本:
+"{text}"
+
+
+
+最终摘要:
+
 """
 
 
