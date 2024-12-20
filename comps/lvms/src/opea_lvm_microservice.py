23ddd8c3aad9167558c9264fe96e02d917b955e7
Spycsh <sihan.chen@intel.com>
2024-12-20 01:40:09 -0800

    add microservice wrapepr

--- comps/lvms/src/opea_lvm_microservice.py
+++ comps/lvms/src/opea_lvm_microservice.py
@@ -3,9 +3,10 @@
 
 import os
 import time
-from typing import List
+from typing import Union, List
 
 from fastapi import File, Form, UploadFile
+from fastapi.responses import StreamingResponse
 from integrations.opea_llava_lvm import OpeaLlavaLvm
 from integrations.opea_tgi_llava_lvm import OpeaTgiLlavaLvm
 from integrations.opea_llama_vision_lvm import OpeaLlamaVisionLvm
@@ -14,9 +15,12 @@ from integrations.opea_video_llama_lvm import OpeaVideoLlamaLvm
 
 
 from comps import (
-    Base64ByteStrDoc,
+    LVMDoc,
+    LVMSearchedMultimodalDoc,
+    LVMVideoDoc,
+    TextDoc,
     CustomLogger,
-    LLMParamsDoc,
+    MetadataTextDoc,
     OpeaComponentController,
     ServiceType,
     opea_microservices,
@@ -24,7 +28,7 @@ from comps import (
     register_statistics,
     statistics_dict,
 )
-from comps.cores.proto.api_protocol import AudioTranscriptionResponse
+
 
 logger = CustomLogger("opea_lvm_microservice")
 logflag = os.getenv("LOGFLAG", False)
@@ -34,66 +38,74 @@ controller = OpeaComponentController()
 
 # Register components
 try:
-    # Instantiate ASR components
-    opea_whisper_asr = OpeaLlavaLvm(
-        name="OpeaWhisperAsr",
-        description="OPEA Whisper ASR Service",
+    # Instantiate LVM components
+    opea_llava_lvm = OpeaLlavaLvm(
+        name="OpeaLlavaLvm",
+        description="OPEA LLaVA LVM Service",
+    )
+    opea_tgi_llava_lvm = OpeaTgiLlavaLvm(
+        name="OpeaTgiLlavaLvm",
+        description="OPEA TGI LLaVA LVM Service",
+    )
+    opea_llama_vision_lvm = OpeaLlamaVisionLvm(
+        name="OpeaLlamaVisionLvm",
+        description="OPEA LLaMA Vison LVM Service",
+    )
+    opea_predictionguard_lvm = OpeaPredictionguardLvm(
+        name="OpeaPredictionguardLvm",
+        description="OPEA PredictionGuard LVM Service",
+    )
+    opea_video_llama_lvm = OpeaVideoLlamaLvm(
+        name="OpeaVideoLlamaLvm",
+        description="OPEA Video LLaMA LVM Service",
     )
 
     # Register components with the controller
-    controller.register(opea_whisper_asr)
+    controller.register(opea_llava_lvm)
+    controller.register(opea_tgi_llava_lvm)
+    controller.register(opea_llama_vision_lvm)
+    controller.register(opea_predictionguard_lvm)
+    controller.register(opea_video_llama_lvm)
 
     # Discover and activate a healthy component
-    controller.discover_and_activate(num_trials=10, timeout=10)
+    controller.discover_and_activate(retries=10, interval=10, timeout=5)
 except Exception as e:
     logger.error(f"Failed to initialize components: {e}")
 
+async def stream_forwarder(response):
+    """Forward the stream chunks to the client using iter_content."""
+    for chunk in response.iter_content(chunk_size=8192):
+        if chunk:
+            yield chunk
 
 @register_microservice(
-    name="opea_service@asr",
-    service_type=ServiceType.ASR,
-    endpoint="/v1/audio/transcriptions",
+    name="opea_service@lvm",
+    service_type=ServiceType.LVM,
+    endpoint="/v1/lvm",
     host="0.0.0.0",
-    port=9099,
-    input_datatype=Base64ByteStrDoc,
-    output_datatype=LLMParamsDoc,
+    port=9399,
 )
-@register_statistics(names=["opea_service@asr"])
-async def audio_to_text(
-    file: UploadFile = File(...),  # Handling the uploaded file directly
-    model: str = Form("openai/whisper-small"),
-    language: str = Form("english"),
-    prompt: str = Form(None),
-    response_format: str = Form("json"),
-    temperature: float = Form(0),
-    timestamp_granularities: List[str] = Form(None),
-) -> AudioTranscriptionResponse:
+@register_statistics(names=["opea_service@lvm"])
+async def lvm(request: Union[LVMDoc, LVMSearchedMultimodalDoc, LVMVideoDoc]) -> Union[TextDoc, MetadataTextDoc, StreamingResponse]:
     start = time.time()
-
-    if logflag:
-        logger.info("ASR file uploaded.")
 
     try:
         # Use the controller to invoke the active component
-        asr_response = await controller.invoke(
-            file=file,
-            model=model,
-            language=language,
-            prompt=prompt,
-            response_format=response_format,
-            temperature=temperature,
-            timestamp_granularities=timestamp_granularities,
-        )
+        lvm_response = await controller.invoke(request)
         if logflag:
-            logger.info(asr_response)
-        statistics_dict["opea_service@asr"].append_latency(time.time() - start, None)
-        return asr_response
+            logger.info(lvm_response)
+        statistics_dict["opea_service@lvm"].append_latency(time.time() - start, None)
+
+        if controller.active_component.name in ['OpeaVideoLlamaLvm']:
+            return StreamingResponse(stream_forwarder(lvm_response))
+
+        return lvm_response
 
     except Exception as e:
-        logger.error(f"Error during asr invocation: {e}")
+        logger.error(f"Error during lvm invocation: {e}")
         raise
 
 
 if __name__ == "__main__":
-    logger.info("OPEA ASR Microservice is starting....")
-    opea_microservices["opea_service@asr"].start()
+    logger.info("OPEA LVM Microservice is starting....")
+    opea_microservices["opea_service@lvm"].start()
