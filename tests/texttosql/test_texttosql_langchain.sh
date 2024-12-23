1ed166bbf9a444144a60ca61a8ddc6ce07678bfa
Yao, Qing <qing.yao@intel.com>
2024-12-23 20:29:36 +0800

    Fix  text2sql UT
    Signed-off-by: Yao, Qing <qing.yao@intel.com>

--- tests/texttosql/test_texttosql_langchain.sh
+++ tests/texttosql/test_texttosql_langchain.sh
@@ -18,19 +18,19 @@ export POSTGRES_DB=chinook
 
 function build_docker_images() {
     cd $WORKPATH
-    docker build --no-cache -t opea/texttosql:comps -f comps/texttosql/langchain/Dockerfile .
+    docker build --no-cache -t opea/texttosql:comps -f comps/texttosql/src/Dockerfile .
 }
 
 function start_service() {
 
-    docker run --name test-texttosql-postgres --ipc=host -e POSTGRES_USER=${POSTGRES_USER} -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_DB=${POSTGRES_DB} -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -p 5442:5432 -d -v $WORKPATH/comps/texttosql/langchain/chinook.sql:/docker-entrypoint-initdb.d/chinook.sql postgres:latest
+    docker run --name test-texttosql-postgres --ipc=host -e POSTGRES_USER=${POSTGRES_USER} -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_DB=${POSTGRES_DB} -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -p 5442:5432 -d -v $WORKPATH/comps/texttosql/src/chinook.sql:/docker-entrypoint-initdb.d/chinook.sql postgres:latest
 
     docker run -d --name="test-texttosql-tgi-endpoint" --ipc=host -p $tgi_port:80 -v ./data:/data --shm-size 1g -e HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN} -e HF_TOKEN=${HF_TOKEN} -e model=${model} ghcr.io/huggingface/text-generation-inference:2.1.0 --model-id $model
 
     export TGI_LLM_ENDPOINT="http://${ip_address}:${tgi_port}"
     texttosql_port=9090
     unset http_proxy
-    docker run -d --name="test-texttosql-server" --ipc=host -p ${texttosql_port}:8090 --ipc=host -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e TGI_LLM_ENDPOINT=$TGI_LLM_ENDPOINT opea/texttosql:comps
+    docker run -d --name="test-texttosql-server" --ipc=host -p ${texttosql_port}:8080 --ipc=host -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e TGI_LLM_ENDPOINT=$TGI_LLM_ENDPOINT opea/texttosql:comps
 
     # check whether tgi is fully ready
     n=0
