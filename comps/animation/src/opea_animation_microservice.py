2a5a5d43693b9c0726a98316fd3efbebd0ef0ba8
Yao, Qing <qing.yao@intel.com>
2024-12-26 14:52:25 +0800

    Rename animation path
    Signed-off-by: Yao, Qing <qing.yao@intel.com>

--- comps/animation/src/opea_animation_microservice.py
+++ comps/animation/src/opea_animation_microservice.py
@@ -10,7 +10,7 @@ import time
 
 # GenAIComps
 from comps import CustomLogger, OpeaComponentController
-from comps.animation.src.integration.opea_animation import OpeaAnimation
+from comps.animation.src.integration.opea import OpeaAnimation
 
 logger = CustomLogger("opea_animation")
 logflag = os.getenv("LOGFLAG", False)
