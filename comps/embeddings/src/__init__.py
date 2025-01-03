a588291e70b645911a9752722ee0f9c489da01e0
lvliang-intel <liang1.lv@intel.com>
2025-01-03 14:02:39 +0800

    update mulimodal embedding
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- comps/embeddings/src/__init__.py
+++ comps/embeddings/src/__init__.py
@@ -1,2 +1,6 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+
+from integrations.opea_tei_embedding import OpeaTEIEmbedding
+from integrations.predictionguard_embedding import PredictionguardEmbedding
+from integrations.opea_multimodal_embedding_bridgetower import OpeaMultimodalEmbeddingBrigeTower
