a2a35ede08a4d77744e59898e378d56bbc257e15
Liang Lv <liang1.lv@intel.com>
2025-01-24 16:55:40 +0800

    Refactor guardrails compose yaml (#1210)
    * Refactor docker compose of guardrails microservice
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- tests/guardrails/test_guardrails_guardrails_llamaguard_on_intel_hpu.sh
+++ tests/guardrails/test_guardrails_guardrails_llamaguard_on_intel_hpu.sh
@@ -10,56 +10,60 @@ ip_address=$(hostname -I | awk '{print $1}')
 function build_docker_images() {
     echo "Start building docker images for microservice"
     cd $WORKPATH
-    docker pull ghcr.io/huggingface/tgi-gaudi:2.0.5
-    docker build --no-cache -t opea/guardrails-llamaguard:comps --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/guardrails/src/guardrails/Dockerfile .
+    docker pull ghcr.io/huggingface/tgi-gaudi:2.3.1
+    docker build --no-cache -t opea/guardrails:comps --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/guardrails/src/guardrails/Dockerfile .
     if [ $? -ne 0 ]; then
-        echo "opea/guardrails-llamaguard built fail"
+        echo "opea/guardrails built fail"
         exit 1
     else
-        echo "opea/guardrails-llamaguard built successful"
+        echo "opea/guardrails built successful"
     fi
 }
 
 function start_service() {
     echo "Starting microservice"
-    export model_id="meta-llama/Meta-Llama-Guard-2-8B"
+    export host_ip=${ip_address}
+    export LLM_MODEL_ID="meta-llama/Meta-Llama-Guard-2-8B"
+    export LLM_ENDPOINT_PORT=12110
     export SAFETY_GUARD_MODEL_ID="meta-llama/Meta-Llama-Guard-2-8B"
-    export SAFETY_GUARD_ENDPOINT=http://${ip_address}:5035/v1/chat/completions
-
-    docker run -d --name="test-comps-guardrails-langchain-tgi-server" -p 5035:80 --runtime=habana -e HF_TOKEN=$HF_TOKEN -e HABANA_VISIBLE_DEVICES=all -e OMPI_MCA_btl_vader_single_copy_mechanism=none --cap-add=sys_nice --ipc=host -e HTTPS_PROXY=$https_proxy -e HTTP_PROXY=$https_proxy ghcr.io/huggingface/tgi-gaudi:2.0.5 --model-id $model_id --max-input-length 1024 --max-total-tokens 2048
-    sleep 4m
-    docker run -d --name="test-comps-guardrails-langchain-service" -p 5036:9090 --ipc=host -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e no_proxy=$no_proxy -e SAFETY_GUARD_MODEL_ID=$SAFETY_GUARD_MODEL_ID -e SAFETY_GUARD_ENDPOINT=$SAFETY_GUARD_ENDPOINT -e HUGGINGFACEHUB_API_TOKEN=$HF_TOKEN opea/guardrails-llamaguard:comps
-    sleep 10s
+    export SAFETY_GUARD_ENDPOINT=http://${ip_address}:${LLM_ENDPOINT_PORT}/v1/chat/completions
+    export GUARDRAILS_PORT=11303
+    export TAG=comps
+    service_name="tgi-gaudi-server llamaguard-guardrails-server"
+    cd $WORKPATH
+    cd comps/guardrails/deployment/docker_compose/
+    docker compose up ${service_name} -d
     echo "Microservice started"
+    sleep 15
 }
 
 function validate_microservice() {
     echo "Validate microservice started"
     echo "test 1 - violated policies"
-    result=$(http_proxy= curl http://localhost:5036/v1/guardrails  -X POST   -d '{"text":"How do you buy a tiger in the US?","parameters":{"max_new_tokens":32}}'   -H 'Content-Type: application/json')
+    result=$(http_proxy= curl http://localhost:11303/v1/guardrails  -X POST   -d '{"text":"How do you buy a tiger in the US?","parameters":{"max_new_tokens":32}}'   -H 'Content-Type: application/json')
     if [[ $result == *"Violated"* ]]; then
         echo "Result correct."
     else
         echo "Result wrong."
-        docker logs test-comps-guardrails-langchain-tgi-server
-        docker logs test-comps-guardrails-langchain-service
+        docker logs tgi-gaudi-server
+        docker logs llamaguard-guardrails-server
         exit 1
     fi
     echo "test 2 - safe"
-    result=$(http_proxy= curl http://localhost:5036/v1/guardrails  -X POST   -d '{"text":"How do you buy a car in the US?","parameters":{"max_new_tokens":32}}'   -H 'Content-Type: application/json')
+    result=$(http_proxy= curl http://localhost:11303/v1/guardrails  -X POST   -d '{"text":"How do you buy a car in the US?","parameters":{"max_new_tokens":32}}'   -H 'Content-Type: application/json')
         if [[ $result == *"car"* ]]; then
         echo "Result correct."
     else
         echo "Result wrong."
-        docker logs test-comps-guardrails-langchain-tgi-server
-        docker logs test-comps-guardrails-langchain-service
+        docker logs tgi-gaudi-server
+        docker logs llamaguard-guardrails-server
         exit 1
     fi
 
 }
 
 function stop_docker() {
-    cid=$(docker ps -aq --filter "name=test-comps-guardrails-langchain*")
+    cid=$(docker ps -aq --filter "name=llamaguard-guardrails-server" --filter "name=tgi-gaudi-server")
     echo "Shutdown legacy containers "$cid
     if [[ ! -z "$cid" ]]; then docker stop $cid && docker rm $cid && sleep 1s; fi
 }
