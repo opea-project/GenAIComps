56a881c84be5b208cf07807b97652bf6dc48df79
letonghan <letong.han@intel.com>
2025-02-13 21:21:02 +0800

    refine opensearch test script
    Signed-off-by: letonghan <letong.han@intel.com>

--- tests/retrievers/test_retrievers_opensearch.sh
+++ tests/retrievers/test_retrievers_opensearch.sh
@@ -14,6 +14,7 @@ WORKPATH=$(dirname "$PWD")
 LOG_PATH="$WORKPATH/tests"
 export host_ip=$(hostname -I | awk '{print $1}')
 service_name="opensearch-vector-db tei-embedding-serving retriever-opensearch"
+retriever_service_name="retriever-opensearch"
 
 function build_docker_images() {
     cd $WORKPATH
@@ -61,13 +62,13 @@ function validate_microservice() {
             echo "[ retriever ] Content is as expected."
         else
             echo "[ retriever ] Content does not match the expected result: $CONTENT"
-            docker logs ${service_name} >> ${LOG_PATH}/retriever.log
+            docker logs ${retriever_service_name} >> ${LOG_PATH}/retriever.log
             docker logs tei-embedding-serving >> ${LOG_PATH}/tei.log
             exit 1
         fi
     else
         echo "[ retriever ] HTTP status is not 200. Received status was $HTTP_STATUS"
-        docker logs ${service_name} >> ${LOG_PATH}/retriever.log
+        docker logs ${retriever_service_name} >> ${LOG_PATH}/retriever.log
         docker logs tei-embedding-serving >> ${LOG_PATH}/tei.log
         exit 1
     fi
@@ -79,14 +80,14 @@ function stop_docker() {
 }
 
 function main() {
-    stop_service
+    stop_docker
 
     build_docker_images
     start_service
 
     validate_microservice
 
-    stop_service
+    stop_docker
     echo y | docker system prune
 }
 
