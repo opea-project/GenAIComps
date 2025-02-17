3b254bff64a1e47a98c8f744611ce35ce7125f56
Spycsh <39623753+Spycsh@users.noreply.github.com>
2025-02-17 11:35:38 +0800

    Fix telemetry connection issue when disabling telemetry (#1290)
    * Fix telemetry connection issue when disabling telemetry
    
    - use ENABLE_OPEA_TELEMETRY to control whether to enable open telemetry, default false
    - fix the issue that logs always show telemetry connection error with each request when telemetry is disabled
    - ban the above error propagation to microservices when telemetry is disabled
    
    Signed-off-by: Spycsh <sihan.chen@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix ut failure where required the flag to be on
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Spycsh <sihan.chen@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Dariusz Trawinski <Dariusz.Trawinski@intel.com>

--- comps/__init__.py
+++ comps/__init__.py
@@ -51,8 +51,7 @@ from comps.cores.mega.orchestrator_with_yaml import ServiceOrchestratorWithYaml
 from comps.cores.mega.micro_service import MicroService, register_microservice, opea_microservices
 
 # Telemetry
-if os.getenv("ENABLE_OPEA_TELEMETRY", "false").lower() == "true":
-    from comps.cores.telemetry.opea_telemetry import opea_telemetry
+from comps.cores.telemetry.opea_telemetry import opea_telemetry
 
 # Common
 from comps.cores.common.component import OpeaComponent, OpeaComponentRegistry, OpeaComponentLoader
