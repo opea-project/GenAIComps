bff8b2a6e7dba06f4012794864f31284496a05a6
jeanyu-habana <jean1.yu@intel.com>
2025-02-19 10:11:44 -0600

    updated test script
    Signed-off-by: jeanyu-habana <jean1.yu@intel.com>

--- tests/third_parties/test_third_parties_neo4j.sh
+++ tests/third_parties/test_third_parties_neo4j.sh
@@ -26,7 +26,7 @@ function deploy_and_start_service() {
     helm repo add neo4j https://helm.neo4j.com/neo4j && helm repo update
 
     #helm install -n neo4j-system --version 5.23.0 graph-neo neo4j/neo4j -f cpu.yaml --set storageClassName=$STORAGE_CLASS_NAME
-    helm install -n neo4j-system --version 5.23.0 graph-neo neo4j/neo4j -f cpu.yaml
+    helm install -n neo4j-system --version 5.23.0 graph-neo neo4j/neo4j --set authEnabled=true --set neo4j.username=neo4j --set neo4j.password=neo4j -f cpu.yaml
     sleep 120s
 }
 
