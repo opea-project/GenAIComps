0e090fa99c2c745986ea683c32b71b95963639d5
jeanyu-habana <jean1.yu@intel.com>
2025-03-01 18:16:49 -0600

    logger
    Signed-off-by: jeanyu-habana <jean1.yu@intel.com>

--- comps/text2cypher/src/integrations/gaudiutils.py
+++ comps/text2cypher/src/integrations/gaudiutils.py
@@ -26,7 +26,9 @@ from optimum.habana.utils import (
 )
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 from transformers.utils import check_min_version
+from comps import CustomLogger
 
+logger = CustomLogger("opea_text2cypher_utils")
 
 def setup_parser(parser):
     # Arguments management
