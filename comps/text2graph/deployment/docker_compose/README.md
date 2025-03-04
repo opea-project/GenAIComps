c5bd5533f9c9da7c26a08400053dacc3cb1c63f8
Raghava, Sharath <sharath.raghava@intel.com>
2025-03-04 00:05:46 +0000

    Changes to checkin text2graph microservice will checkin first native and add support for Gaudi later
    Signed-off-by: Raghava, Sharath <sharath.raghava@intel.com>

---
+++ comps/text2graph/deployment/docker_compose/README.md
@@ -0,0 +1,2 @@
+update setup_service_env.sh with variables and source before starting
+the script is in the comps/text2graph/src dir. set the variables at start
---
+++ tests/text2graph/native_text2graph.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+#WORKPATH=$(dirname "$PWD")
+WORKPATH=$(git rev-parse --show-toplevel)
+
+echo $WORKPATH
+ip_address=$(hostname -I | awk '{print $1}')
+service_name="text2graph"
+
+function build_docker_graph() {
+    echo "===================  START BUILD DOCKER ========================"
+    cd $WORKPATH
+    echo $(pwd)
+    docker build --no-cache -t opea/text2graph:latest -f comps/text2graph/src/Dockerfile .
+    if [ $? -ne 0 ]; then
+        echo "opea/text2graph built fail"
+        exit 1
+    else
+        echo "opea/text2graph built successful"
+    fi
+    echo "===================  END BUILD DOCKER ========================"
+}
+
+function start_service() {
+    echo "===================  START SERVICE ========================"
+    cd $WORKPATH/comps/text2graph/deployment/docker_compose
+    docker compose -f compose.yaml up ${service_name} -d > start_services_with_compose.log
+    sleep 10s
+    echo "===================  END SERVICE ========================"
+}
+
+function validate_microservice() {
+    echo "===================  START VALIDATE ========================"
+    cd $WORKPATH/tests/text2graph
+    python3 example_from_file.py
+    echo "===================  END VALIDATE ========================"
+}
+
+function stop_docker() {
+    echo "===================  START STOP DOCKER ========================"
+    cd $WORKPATH/comps/text2graph/deployment/docker_compose
+    docker compose -f compose.yaml down ${service_name} --remove-orphans
+    echo "===================  END STOP DOCKER ========================"
+}
+
+function main() {
+
+    stop_docker
+
+    build_docker_graph
+    start_service
+    validate_microservice
+
+    stop_docker
+    echo y | docker system prune
+
+}
+
+main
