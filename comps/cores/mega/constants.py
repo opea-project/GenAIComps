94ca26c8d43ca1da81fd9ed0708029d4697c6f64
Jean Yu <jean1.yu@intel.com>
2025-03-09 22:46:10 -0500

    add text2cypher component (#1319)
    Signed-off-by: jeanyu-habana <jean1.yu@intel.com>

--- comps/cores/mega/constants.py
+++ comps/cores/mega/constants.py
@@ -35,6 +35,7 @@ class ServiceType(Enum):
     IMAGE2IMAGE = 18
     TEXT2SQL = 19
     TEXT2GRAPH = 20
+    TEXT2CYPHER = 21
 
 
 class MegaServiceEndpoint(Enum):
@@ -54,6 +55,7 @@ class MegaServiceEndpoint(Enum):
     RETRIEVALTOOL = "/v1/retrievaltool"
     FAQ_GEN = "/v1/faqgen"
     GRAPH_RAG = "/v1/graphrag"
+    HYBRID_RAG = "/v1/hybridrag"
     # Follow OPENAI
     EMBEDDINGS = "/v1/embeddings"
     TTS = "/v1/audio/speech"
