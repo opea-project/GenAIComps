fbd3c7f3abc55e6086a07878378100fca91fcb83
chensuyue <suyue.chen@intel.com>
2025-03-19 12:26:59 +0800

    bug fix
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .github/workflows/scripts/get_test_matrix.sh
+++ .github/workflows/scripts/get_test_matrix.sh
@@ -59,7 +59,7 @@ function find_test_1() {
                         fill_in_matrix "$find_test"
                     fi
                 fi
-            elif [[ $(ls ${service_path} | grep "third_parties") ]]; then
+            elif [[ $(echo ${service_path} | grep "third_parties") ]]; then
                  # new org with `src` and `third_parties` folder
                 service_name=$(echo $service_path | sed 's:/src::' | tr '/' '_' | cut -c7-) # comps/third_parties/vllm/src -> third_parties_vllm
                 find_test=$(find ./tests -type f -name test_${service_name}*.sh) || true
