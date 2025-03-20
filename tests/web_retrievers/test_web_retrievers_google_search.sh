341b5f9b68ea0acc1b1af734db75fe0dbd005161
ZePan110 <ze.pan@intel.com>
2025-03-20 21:13:44 +0800

    Use model cache for docker compose test.
    Signed-off-by: ZePan110 <ze.pan@intel.com>

--- tests/web_retrievers/test_web_retrievers_google_search.sh
+++ tests/web_retrievers/test_web_retrievers_google_search.sh
@@ -9,7 +9,7 @@ ip_address=$(hostname -I | awk '{print $1}')
 export TAG=comps
 export WEB_RETRIEVER_PORT=11900
 export TEI_PORT=11901
-
+export DATA_PATH=${model_cache}
 function build_docker_images() {
     cd $WORKPATH
     docker build --no-cache -t opea/web-retriever:$TAG --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/web_retrievers/src/Dockerfile .
