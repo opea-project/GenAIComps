2cb9b0b5f586c79a4176a752a59e9c5f364ad656
chen, suyue <suyue.chen@intel.com>
2025-03-25 11:52:05 +0800

    Update default model cache for new CI cluster (#1445)
    Signed-off-by: chensuyue <suyue.chen@intel.com>
    Signed-off-by: jilongwa <jilong.wang@intel.com>

--- tests/llms/test_llms_faq-generation_tgi_on_intel_hpu.sh
+++ tests/llms/test_llms_faq-generation_tgi_on_intel_hpu.sh
@@ -34,7 +34,7 @@ function start_service() {
     export LLM_ENDPOINT="http://${host_ip}:${LLM_ENDPOINT_PORT}"
     export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
     export LOGFLAG=True
-    export DATA_PATH="/data2/cache"
+    export DATA_PATH="/data2/hf_model"
 
     cd $WORKPATH/comps/llms/deployment/docker_compose
     docker compose -f compose_faq-generation.yaml up ${service_name} -d > ${LOG_PATH}/start_services_with_compose.log
