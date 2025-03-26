71c946dc1b091ffe6b677e4be09f62767bf6791e
ZePan110 <ze.pan@intel.com>
2025-03-27 00:51:22 +0800

    Use model cache for third_parties docker compose test.
    Signed-off-by: ZePan110 <ze.pan@intel.com>

--- tests/third_parties/test_third_parties_vllm_openvino_on_intel_arc.sh
+++ tests/third_parties/test_third_parties_vllm_openvino_on_intel_arc.sh
@@ -30,7 +30,7 @@ function build_container() {
 # Function to start Docker container
 start_container() {
     export LLM_ENDPOINT_PORT=12206
-    export HF_CACHE_DIR=$HOME/.cache/huggingface
+    export HF_CACHE_DIR=${model_cache:-./data}
     export RENDER_GROUP_ID=110
     export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
 
@@ -89,7 +89,7 @@ function test_api_endpoint {
 
 function stop_docker() {
     cd $WORKPATH/comps/llms/deployment/docker_compose
-    docker compose -f compose_faq-generation.yaml down --remove-orphans
+    docker compose -f compose_faq-generation.yaml down ${service_name} --remove-orphans
 }
 
 # Main function
