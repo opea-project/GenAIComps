0727384606ba212a838e479895506d5a2f15c704
Spycsh <sihan.chen@intel.com>
2025-03-25 21:13:56 -0700

    integrate UI-TARS vLLM in lvm component

--- comps/lvms/src/integrations/vllm.py
+++ comps/lvms/src/integrations/vllm.py
@@ -45,6 +45,65 @@ class ChatTemplate:
 
         return template.format(context=context, question=question)
 
+    @staticmethod
+    def generate_ui_tars_prompt_for_computer(prompt: str):
+        template = r"""You are a GUI agent. You are given a task and your action history, with screenshots. You need to perform the next action to complete the task.
+
+        ## Output Format
+        ```\nThought: ...
+        Action: ...\n```
+
+        ## Action Space
+
+        click(start_box='<|box_start|>(x1,y1)<|box_end|>')
+        left_double(start_box='<|box_start|>(x1,y1)<|box_end|>')
+        right_single(start_box='<|box_start|>(x1,y1)<|box_end|>')
+        drag(start_box='<|box_start|>(x1,y1)<|box_end|>', end_box='<|box_start|>(x3,y3)<|box_end|>')
+        hotkey(key='')
+        type(content='') #If you want to submit your input, use \"\
+        \" at the end of `content`.
+        scroll(start_box='<|box_start|>(x1,y1)<|box_end|>', direction='down or up or right or left')
+        wait() #Sleep for 5s and take a screenshot to check for any changes.
+        finished()
+        call_user() # Submit the task and call the user when the task is unsolvable, or when you need the user's help.
+
+
+        ## Note
+        - Use Chinese in `Thought` part.
+        - Summarize your next action (with its target element) in one sentence in `Thought` part.
+
+        ## User Instruction
+        """
+
+        return template + prompt
+
+    @staticmethod
+    def generate_ui_tars_prompt_for_mobile(prompt: str):
+        template = r"""You are a GUI agent. You are given a task and your action history, with screenshots. You need to perform the next action to complete the task.
+
+        ## Output Format
+        ```\nThought: ...
+        Action: ...\n```
+
+        ## Action Space
+        click(start_box='<|box_start|>(x1,y1)<|box_end|>')
+        long_press(start_box='<|box_start|>(x1,y1)<|box_end|>', time='')
+        type(content='')
+        scroll(start_box='<|box_start|>(x1,y1)<|box_end|>', end_box='<|box_start|>(x3,y3)<|box_end|>')
+        press_home()
+        press_back()
+        finished(content='') # Submit the task regardless of whether it succeeds or fails.
+
+        ## Note
+        - Use English in `Thought` part.
+
+        - Write a small plan and finally summarize your next action (with its target element) in one sentence in `Thought` part.
+
+        ## User Instruction
+        """
+
+        return template + prompt
+
 
 @OpeaComponentRegistry.register("OPEA_VLLM_LVM")
 class OpeaVllmLvm(OpeaComponent):
@@ -115,6 +174,11 @@ class OpeaVllmLvm(OpeaComponent):
             temperature = request.temperature
             # top_k = request.top_k
             top_p = request.top_p
+
+        # update ui_tars prompt
+        if "UI-TARS" in LLM_MODEL_ID:
+            # TODO validate mobile flow https://github.com/bytedance/UI-TARS
+            prompt = ChatTemplate.generate_ui_tars_prompt_for_computer(prompt=prompt)
 
         if not img_b64_str:
             # If img_b64_str was an empty string, which means we have just have a text prompt.
