dd146fe7cfa155d8bf8a34027058ffe0ded2c256
jilongW <109333127+jilongW@users.noreply.github.com>
2025-03-27 16:07:33 +0800

    Rebase
    * change to latest
    
    * Revert "[pre-commit.ci] auto fixes from pre-commit.com hooks"
    
    This reverts commit cd59ab9769536af5d5694a922797362fa87d1d7b.
    
    Signed-off-by: jilongwa <jilong.wang@intel.com>

--- comps/finetuning/src/integrations/xtune/install_xtune.sh
+++ comps/finetuning/src/integrations/xtune/install_xtune.sh
@@ -8,16 +8,16 @@ if [ -f "README.md" ]; then
 else
     echo "prepare dassl for xtune"
     git clone https://github.com/KaiyangZhou/Dassl.pytorch.git dassl
-    cd dassl && git apply --reject ../dassl-update-for-xtune.patch && cd ..
+    cd dassl && git am ../dassl-update-for-xtune.patch && cd ..
     mv dassl clip_finetune/
     echo "dassl done"
     echo "prepare adaclip for xtune"
     git clone https://github.com/SamsungLabs/AdaCLIP.git
-    cd AdaCLIP && git apply --reject ../adaclip-update-for-xtune.patch && cd .. && rsync -avPr AdaCLIP/  adaclip_finetune/ && rm -rf AdaCLIP
+    cd AdaCLIP && git am ../adaclip-update-for-xtune.patch && cd .. && rsync -avPr AdaCLIP/  adaclip_finetune/ && rm -rf AdaCLIP
     echo "adaclip done"
     echo "prepare llama-factory for xtune"
     git clone https://github.com/hiyouga/LLaMA-Factory.git
-    cd LLaMA-Factory && git checkout 38e955d4a917a7e600cfd17a041a1472e8d81370 && git apply --reject ../update_for_xtune.patch && cd ..
+    cd LLaMA-Factory && git checkout 38e955d4a917a7e600cfd17a041a1472e8d81370 && git am ../update_for_xtune.patch && cd ..
     rsync -avPr LLaMA-Factory/  .
     rm -rf LLaMA-Factory
     mv clip_finetune src/llamafactory/
