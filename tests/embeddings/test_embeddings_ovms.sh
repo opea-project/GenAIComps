8149be553b8661ab394faaf3a0a5ab3a28a00115
chensuyue <suyue.chen@intel.com>
2025-03-28 23:10:02 +0800

    for test
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- tests/embeddings/test_embeddings_ovms.sh
+++ tests/embeddings/test_embeddings_ovms.sh
@@ -75,6 +75,7 @@ function validate_microservice() {
 
 function validate_microservice_with_openai() {
     ovms_service_port=10205
+    echo $PATH
     pip install openai
     python3 ${WORKPATH}/tests/utils/validate_svc_with_openai.py $ip_address $ovms_service_port "embedding"
     if [ $? -ne 0 ]; then
