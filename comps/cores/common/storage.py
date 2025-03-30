8a6cdcd9901609e2f8e2b0ecb95c5501327fd17e
Anthony Mahanna <anthony.mahanna@arangodb.com>
2025-03-29 21:02:57 -0400

    initial commit
    Signed-off-by: Anthony Mahanna <anthony.mahanna@gmail.com>

---
+++ comps/cores/common/storage.py
@@ -0,0 +1,150 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from abc import ABC, abstractmethod
+
+from ..mega.logger import CustomLogger
+
+logger = CustomLogger("OpeaStore")
+
+
+class OpeaStore(ABC):
+    """The OpeaStore class serves as the base class for all Storage APIs.
+    It provides a unified interface and foundational attributes that every derived Storage API inherits and extends.
+
+    Attributes:
+        name (str): The name of the component (e.g 'arangodb', 'redis', 'mongodb', etc.)
+        description (str): A brief description of the component's functionality.
+        config (dict): A dictionary containing configuration parameters for the component.
+    """
+
+    def __init__(self, name: str, description: str, config: dict = None):
+        """Initializes an OpeaComponent instance with the provided attributes.
+
+        Args:
+            name (str): The name of the component.
+            description (str): A brief description of the component.
+            config (dict, optional): Configuration parameters for the component. Defaults to an empty dictionary.
+        """
+        self.name = name
+        self.description = description
+        self.config = config if config is not None else {}
+
+    def get_meta(self) -> dict:
+        """Retrieves metadata about the component, including its name, type, description, and configuration.
+
+        Returns:
+            dict: A dictionary containing the component's metadata.
+        """
+        return {
+            "name": self.name,
+            "description": self.description,
+            "config": self.config,
+        }
+
+    def update_config(self, key: str, value):
+        """Updates a configuration parameter for the component.
+
+        Args:
+            key (str): The configuration parameter's key.
+            value: The new value for the configuration parameter.
+        """
+        self.config[key] = value
+
+    def __repr__(self):
+        """Provides a string representation of the component for debugging and logging purposes.
+
+        Returns:
+            str: A string representation of the OpeaComponent instance.
+        """
+        return f"OpeaStore(name={self.name}, type={self.type}, description={self.description})"
+
+    @abstractmethod
+    def save_document(self, doc: dict) -> None:
+        """
+        Save a single document to the store.
+        Document can optionally contain a unique identifier.
+
+        Args:
+            doc (dict): The document data to save.
+        """
+        raise NotImplementedError("save_document method must be implemented by subclasses.")
+
+    @abstractmethod
+    def save_documents(self, docs: list[dict]) -> None:
+        """
+        Save multiple documents to the store.
+        Documents can optionally contain unique identifiers.
+
+        Args:
+            docs (list[dict]): A list of document data to save.
+        """
+        raise NotImplementedError("save_documents method must be implemented by subclasses.")
+
+    @abstractmethod
+    def update_document(self, doc: dict) -> None:
+        """
+        Update a single document in the store.
+        Document must contain its unique identifier.
+
+        Args:
+            doc (dict): The document data to update.
+        """
+        raise NotImplementedError("update_document method must be implemented by subclasses.")
+
+    @abstractmethod
+    def update_documents(self, docs: list[dict]) -> None:
+        """
+        Update multiple documents in the store.
+        Each document must contain its unique identifier.
+
+        Args:
+            docs (list[dict]): The list of documents to update.
+        """
+        raise NotImplementedError("update_documents method must be implemented by subclasses.")
+
+    @abstractmethod
+    def get_document_by_id(self, id: str) -> dict:
+        """
+        Retrieve a single document by its unique identifier.
+
+        Args:
+            id (str): The unique identifier for the document.
+
+        Returns:
+            dict: The retrieved document data.
+        """
+        raise NotImplementedError("get_document_by_id method must be implemented by subclasses.")
+
+    @abstractmethod
+    def get_documents_by_ids(self, ids: list[str]) -> list[dict]:
+        """
+        Retrieve multiple documents by their unique identifiers.
+
+        Args:
+            ids (list[str]): A list of unique identifiers for the documents.
+
+        Returns:
+            list[dict]: A list of retrieved document data.
+        """
+        raise NotImplementedError("get_documents_by_ids method must be implemented by subclasses.")
+
+    @abstractmethod
+    def delete_document(self, id: str) -> None:
+        """
+        Delete a single document from the store.
+
+        Args:
+            id (str): The unique identifier for the document.
+        """
+        raise NotImplementedError("delete_document method must be implemented by subclasses.")
+
+    @abstractmethod
+    def delete_documents(self, ids: list[str]) -> None:
+        """
+        Delete multiple documents from the store.
+
+        Args:
+            ids (list[str]): A list of unique identifiers for the documents.
+        """
+        raise NotImplementedError("delete_documents method must be implemented by subclasses.")
