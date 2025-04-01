38ed7e29cd19e1924c093f04be503b7326d90668
jilongwa <jilong.wang@intel.com>
2025-04-01 10:37:10 +0800

    reduce file
    Signed-off-by: jilongwa <jilong.wang@intel.com>
    Signed-off-by: jlwang <jilong.wang@intel.com>

--- comps/finetuning/src/integrations/xtune/llama_factory/src/llamafactory/train/sft_adaclip/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2025 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-from .workflow import run_sft_adaclip
-
-
-__all__ = ["run_sft_adaclip"]
--- comps/finetuning/src/integrations/xtune/llama_factory/src/llamafactory/train/sft_clip/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2025 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-from .workflow import run_sft_clip
-
-
-__all__ = ["run_sft_clip"]
