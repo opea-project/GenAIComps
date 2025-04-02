2a4abfdcfc6faf23b3453b8c9eb28bdd9b55c822
Anthony Mahanna <anthony.mahanna@gmail.com>
2025-04-02 11:03:49 -0400

    cleanup: `opea_store`
    Signed-off-by: Anthony Mahanna <anthony.mahanna@arangodb.com>

--- comps/cores/storages/models.py
+++ comps/cores/storages/models.py
@@ -1,27 +1,8 @@
-# Copyright (C) 2025 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
 from typing import Annotated, Optional
 
 from pydantic import BaseModel, Field
 
 from comps.cores.proto.api_protocol import ChatCompletionRequest
-
-from .arangodb import ArangoDBStore
-
-# from .redisdb import RedisDBStore
-# from .mongodb import MongoDBStore
-
-
-def opea_store(name: str, *args, **kwargs):
-    if name == "arangodb":
-        return ArangoDBStore(name, *args, **kwargs)
-    # elif name == "redis":
-    #     return RedisDBStore(*args, **kwargs)
-    # elif name == "mongodb":
-    #     return MongoDBStore(*args, **kwargs)
-    else:
-        raise ValueError(f"Unknown Data Store: {name}")
 
 
 #################################################################
