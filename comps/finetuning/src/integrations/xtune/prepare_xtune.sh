5ee4a1f4329ed023f2c3c3642083d495548e0622
jilongwa <jilong.wang@intel.com>
2025-04-07 21:52:59 +0800

    fix log output location is wrong
    Signed-off-by: jilongwa <jilong.wang@intel.com>

--- comps/finetuning/src/integrations/xtune/prepare_xtune.sh
+++ comps/finetuning/src/integrations/xtune/prepare_xtune.sh
@@ -18,13 +18,13 @@ else
     echo "prepare for xtune done"
     echo "install requirements"
     python -m pip install --no-cache-dir -r requirements.txt
+    pip uninstall -y torchvision torchaudio
     python -m pip install --no-cache-dir torch==2.6.0+xpu torchvision torchaudio --index-url https://download.pytorch.org/whl/xpu
     cd src/llamafactory/clip_finetune/dassl
     python setup.py develop
     cd ../../../..
     pip install matplotlib
     pip install -e ".[metrics]"
-    pip install --no-deps transformers==4.45.0 datasets==2.21.0 accelerate==0.34.2 peft==0.12.0
     pip install --no-cache-dir --force-reinstall intel-extension-for-pytorch==2.6.10+xpu oneccl_bind_pt==2.6.0+xpu --extra-index-url https://pytorch-extension.intel.com/release-whl/stable/xpu/us/
     echo "start llamafactory webui"
     ZE_AFFINITY_MASK=0 llamafactory-cli webui &
