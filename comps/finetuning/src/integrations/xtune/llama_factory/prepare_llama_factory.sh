1d0fb1fff733d31fc373dd8e6f577cd641616826
jilongwa <jilong.wang@intel.com>
2025-04-07 21:23:25 +0800

    update

--- comps/finetuning/src/integrations/xtune/llama_factory/prepare_llama_factory.sh
+++ comps/finetuning/src/integrations/xtune/llama_factory/prepare_llama_factory.sh
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 echo "prepare llama-factory for xtune"
-git clone https://github.com/hiyouga/LLaMA-Factory.git
-cd LLaMA-Factory && git fetch origin pull/7519/head:xtune && git checkout xtune && cd ..
+git clone https://github.com/jilongW/LLaMA-Factory.git
+cd LLaMA-Factory && git checkout xtune && cd ..
 rsync -avPr LLaMA-Factory/  .
 rm -rf LLaMA-Factory
 echo "prepare llama-factory done"
