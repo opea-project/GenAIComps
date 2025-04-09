1ff8a037e0484a76c57ce62b27c3ed5d9a87d29f
jilongW <109333127+jilongW@users.noreply.github.com>
2025-04-09 09:08:24 +0800

    Fix xtune output location is wrong and update doc (#1533)
    Signed-off-by: jilongwa <jilong.wang@intel.com>
    Signed-off-by: Chingis Yundunov <c.yundunov@datamonsters.com>

--- comps/finetuning/src/integrations/xtune/llama_factory/prepare_llama_factory.sh
+++ comps/finetuning/src/integrations/xtune/llama_factory/prepare_llama_factory.sh
@@ -4,7 +4,7 @@
 
 echo "prepare llama-factory for xtune"
 git clone https://github.com/hiyouga/LLaMA-Factory.git
-cd LLaMA-Factory && git fetch origin pull/7519/head:xtune && git checkout xtune && cd ..
+cd LLaMA-Factory && git fetch origin pull/7625/head:xtune && git checkout xtune && cd ..
 rsync -avPr LLaMA-Factory/  .
 rm -rf LLaMA-Factory
 echo "prepare llama-factory done"
--- comps/finetuning/src/integrations/xtune/prepare_xtune.sh
+++ comps/finetuning/src/integrations/xtune/prepare_xtune.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 # Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
+GUI=$1
 if [ -f "done" ]; then
     echo "All component preparation is done"
     echo "Please follow README.md to install driver and other dependency"
+    if [ -z $GUI ]; then
+        ps aux | grep "llamafactory-cli webui" | head -n 1 | awk '{print $2}' |xargs kill
+        ZE_AFFINITY_MASK=0 llamafactory-cli webui &
+        if [ $? -eq 0 ]; then
+            echo "server start successfully"
+        else
+            echo "failed to start server, please check your environment"
+        fi
+    else
+        ps aux | grep "llamafactory-cli webui" | head -n 1 | awk '{print $2}' |xargs kill
+        echo "kill webui service done"
+    fi
 else
     echo "start prepare for xtune"
     bash clip_finetune/prepare_clip_finetune.sh
@@ -18,20 +30,25 @@ else
     echo "prepare for xtune done"
     echo "install requirements"
     python -m pip install --no-cache-dir -r requirements.txt
+    pip uninstall -y torchvision torchaudio
     python -m pip install --no-cache-dir torch==2.6.0+xpu torchvision torchaudio --index-url https://download.pytorch.org/whl/xpu
     cd src/llamafactory/clip_finetune/dassl
     python setup.py develop
     cd ../../../..
     pip install matplotlib
     pip install -e ".[metrics]"
-    pip install --no-deps transformers==4.45.0 datasets==2.21.0 accelerate==0.34.2 peft==0.12.0
     pip install --no-cache-dir --force-reinstall intel-extension-for-pytorch==2.6.10+xpu oneccl_bind_pt==2.6.0+xpu --extra-index-url https://pytorch-extension.intel.com/release-whl/stable/xpu/us/
     echo "start llamafactory webui"
-    ZE_AFFINITY_MASK=0 llamafactory-cli webui &
-    if [ $? -eq 0 ]; then
-        echo "server start successfully"
+    if [ -z $GUI ]; then
+        ZE_AFFINITY_MASK=0 llamafactory-cli webui &
+        if [ $? -eq 0 ]; then
+            echo "server start successfully"
+        else
+            echo "failed to start server, please check your environment"
+        fi
     else
-        echo "failed to start server, please check your environment"
+        ps aux | grep "llamafactory-cli webui" | head -n 1 | awk '{print $2}' |xargs kill
+        echo "kill webui service done"
     fi
     echo 0 >> done
     echo "Please follow README.md to install driver or update torch lib"
