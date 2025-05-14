66b7dcbd646e820ef75861e3893553d65aeba055
lvliang-intel <liang1.lv@intel.com>
2025-05-14 13:41:38 +0800

    Fix dataprep opensearch ingest issue
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- comps/dataprep/src/integrations/opensearch.py
+++ comps/dataprep/src/integrations/opensearch.py
@@ -108,6 +108,9 @@ class OpeaOpenSearchDataprep(OpeaComponent):
         self.opensearch_client = OpenSearchVectorSearch(
             opensearch_url=OPENSEARCH_URL,
             index_name=Config.INDEX_NAME,
+            # Default engine for OpenSearch is "nmslib", 
+            # but "nmslib" engine is deprecated in OpenSearch and cannot be used for new index creation in OpenSearch from 3.0.0.
+            engine="faiss",
             embedding_function=self.embeddings,
             http_auth=self.auth,
             use_ssl=True,
