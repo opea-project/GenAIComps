ba615c1b283023e967e1ba2cbab86608658273da
Yao, Qing <qing.yao@intel.com>
2025-05-22 14:51:26 +0800

    Add MCP UT for animation/image2image/retriever.
    Signed-off-by: Yao, Qing <qing.yao@intel.com>

--- comps/image2image/src/opea_image2image_microservice.py
+++ comps/image2image/src/opea_image2image_microservice.py
@@ -18,7 +18,7 @@ from comps import (
     statistics_dict,
 )
 from comps.cores.mega.constants import MCPFuncType
-
+from comps.image2image.src.integrations.native import OpeaImageToImage
 args = None
 enable_mcp = os.getenv("ENABLE_MCP", "").strip().lower() in {"true", "1", "yes"}
 
