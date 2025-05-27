a1770fead113019ac66e4bbce37e45bc664739e5
Ed Lee <16417837+edlee123@users.noreply.github.com>
2025-05-27 13:09:14 -0500

    Fake test to check CI
    Signed-off-by: Ed Lee <16417837+edlee123@users.noreply.github.com>

---
+++ tests/llms/test_llms_do_nothing.sh
@@ -0,0 +1,8 @@
+
+
+echo "Please check CI for
+tests/llms/test_llms_faq-generation_vllm_on_intel_hpu.sh
+tests/llms/test_llms_text-generation_native_phi4_on_intel_hpu.sh
+tests/llms/test_llms_text-generation_native_phi4_multimodal_on_intel_hpu.sh
+
+Do unpinned dependencies in src/text-generation/requirements.txt work with gaudi 1.20.1 container?"
