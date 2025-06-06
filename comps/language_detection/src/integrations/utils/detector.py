876ccec3912e4f2835201d97d4d48ad8a3e6ead0
Ye, Xinyu <xinyu.ye@intel.com>
2025-06-06 01:40:00 -0400

    upstream E-RAG's language detection microservice.
    Signed-off-by: Ye, Xinyu <xinyu.ye@intel.com>

---
+++ comps/language_detection/src/integrations/utils/detector.py
@@ -0,0 +1,13 @@
+from ftlangdetect import detect
+
+
+def detect_language(input_text):
+    """
+        Detects the language of input text.
+        Uses language detection model to get detected language.
+    """
+    input_text = input_text.replace("\n"," ")
+    detection = detect(text=input_text, low_memory=True)
+    detected_lang = detection['lang']
+    
+    return detected_lang
