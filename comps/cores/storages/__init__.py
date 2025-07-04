a206b4733022e6172ba9b7c6ff48625951bb2426
WenjiaoYue <wenjiao.yue@intel.com>
2025-07-04 15:42:16 +0800

    Porting Redis to OPEAStore (#1818)
    * Porting Redis to OPEAStore
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add dependencies and modify corresponding scripts
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    fix CI issue
    
    Signed-off-by: WenjiaoYue <wenjiao.yue@intel.com>
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    add aupdate_document script
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Update and specify the version number
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Upgrade Redis version and expand test coverage
    
    Signed-off-by: WenjiaoYue <wenjiao.yue@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat(redis): fix pre-commit.ci issues and improve test coverage
    
    - Resolved lint errors from pre-commit.ci
    - Added and updated test cases for RedisDBStore
    - Improved coverage for aupdate_document and error cases
    
    ---------
    
    Signed-off-by: Ubuntu <azureuser@denvr-inf.kifxisxbiwme5gt4kkwqsfdjuh.dx.internal.cloudapp.net>
    
    Upgrade Redis version and expand test coverage
    
    Signed-off-by: WenjiaoYue <wenjiao.yue@intel.com>
    
    fix pre-commit.ci errors and increase RedisDBStore test coverage
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: WenjiaoYue <wenjiao.yue@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- comps/cores/storages/__init__.py
+++ comps/cores/storages/__init__.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .arangodb import ArangoDBStore
+from .redisdb import RedisDBStore
 
 # from .redisdb import RedisDBStore
 from .mongodb import MongoDBStore
@@ -10,8 +11,8 @@ from .mongodb import MongoDBStore
 def opea_store(name: str, *args, **kwargs):
     if name == "arangodb":
         return ArangoDBStore(name, *args, **kwargs)
-    # elif name == "redis":
-    #     return RedisDBStore(*args, **kwargs)
+    elif name == "redis":
+        return RedisDBStore(name, *args, **kwargs)
     elif name == "mongodb":
         return MongoDBStore(name, *args, **kwargs)
     else:
